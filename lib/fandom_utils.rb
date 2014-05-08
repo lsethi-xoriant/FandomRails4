0bc109b7d01e717d9935794de5f4f939a052f3a0
Diego Dainese <diego@MacBook-Pro-di-Diego.local>
2014-05-08 12:09:09 +0200

    [111] added an utility to handle browser and frontend caching

--- lib/fandom_utils.rb
+++ lib/fandom_utils.rb
@@ -20,6 +20,24 @@ module FandomUtils
       prepend_view_path "#{Rails.root}/site/#{site.id}/views"
     end
   end
+  
+  # set browser and frontend caching; the page is cached by the reverse proxy if it's public and it's not authenticated; 
+  # beware: in this case the cookies (hence the session) are completely unset
+  # pages with form should never be cached, as they contain the CSRF token that should match with the user session   
+  def cache_control(seconds, is_public=false)
+    if request.method == 'GET' || request.method == 'HEAD' 
+      # this would be useful for akamai
+      #headers['Edge-control'] = "!no-store, max-age=#{seconds/60}"
+      
+      # this sets the Cache-Control header, that drives browser caching
+      expires_in seconds, :public => is_public, 'max-stale' => 0
+      
+      if is_public && current_user.nil?
+        env['rack.session.options'][:skip] = true
+        response.headers.delete('Set-Cookie')
+      end
+    end
+  end
 
   # Can be used as a constrains in routes.rb to define site-specific routes.
   class SiteMatcher
