8d77c6d9d11eaf347eac34adfd0e369eaf45d18b
Diego Dainese <diego@MacBook-Pro-di-Diego.local>
2014-05-21 16:36:38 +0200

    [MB16] Maxibon configured for the domain maxibon.shado.tv

--- config/sites/maxibon.rb
+++ config/sites/maxibon.rb
@@ -2,7 +2,7 @@ module Fandom
   class Application < Rails::Application
     register_fandom_site(
       id: 'maxibon', 
-      domains: ['maxibon.fandom.localdomain'],
+      domains: ['maxibon.fandom.localdomain', 'maxibon.shado.tv'],
       assets_precompile: ['maxibon_application.css', 'maxibon_application.js']
     )
   end
