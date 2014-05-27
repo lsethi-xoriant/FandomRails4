722bc9f0a3af4257fcbcad496d2c5d9ae268b53f
Alessandro Tolomio <alessandrotolomio@MacBook-Pro-di-Alessandro.local>
2014-05-27 11:34:04 +0200

    [MB29] Initialize extra page

--- config/sites/maxibon.rb
+++ config/sites/maxibon.rb
@@ -6,8 +6,7 @@ module Fandom
       assets_precompile: ['maxibon_application.css', 'maxibon_application.js'],
       environment: { 
         'EMAIL_ADDRESS' => 'noreply@maxibon.it', 
-        'FACEBOOK_APP_ID' => '1480351255532528', 
-        'FACEBOOK_APP_SECRET' => '4399a5a802c95ac8ed7b2ed812e5c55a' }
+      }
     )
   end
 end
