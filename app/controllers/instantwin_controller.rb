db6fe1e877e968d421a3d9ec0109b1dbabcc6a02
Matteo Pozzana <mpozzana@3pstudio.it>
2014-05-27 18:36:11 +0200

    [47] Instantwin clear code

--- app/controllers/instantwin_controller.rb
+++ app/controllers/instantwin_controller.rb
@@ -5,23 +5,20 @@ class InstantwinController < ApplicationController
   layout "admin"
   DAYS_IN_MONTH = [nil, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
   
-  #
   # Returns days in a month
   #
   # month - month want to know days amount
-  # year - specific year
-  # 
+  # year - specific year 
   def days_in_month(month, year = Time.now.year)
    return 29 if month == 2 && Date.gregorian_leap?(year)
    DAYS_IN_MONTH[month]
   end
 
-  #TODO MAXIBON
+  # TODO: maxibon
   #
   # play a ticket for current user on the contest
   #
   # contest_id - id of the contest
-  #
   def play_ticket_mb
     @ticketended = false
     @win = false
@@ -34,8 +31,8 @@ class InstantwinController < ApplicationController
 
         @contest_points = @contest_points.first
 
-        time_current = Time.now.in_time_zone("Rome")
-        if !checkWin_mb(time_current,contest)
+        time_current = Time.now.utc
+        if !check_win_mb(time_current,contest)
 
           PlayticketEvent.create(:points_spent => contest.conversion_rate, :used_at => time_current, :winner => false, 
                                   :user_id => current_user.id)
@@ -56,33 +53,36 @@ class InstantwinController < ApplicationController
     end
   end
   
-  #TODO MAXIBON
+  # TODO: maxibon
   #
   # check if a ticket is winner
   #
   # ctime - timestamp of played ticket
   # contest - contest for which the ticket is used
   #
-  def checkWin_mb(ctime,contest)
+  def check_win_mb(ctime,contest)
     contest.contest_periodicities.each do |cp|
-      iw = Instantwin.where("contest_periodicity_id = ? AND instantwins.time_to_win<?",cp.id,ctime).order("instantwins.time_to_win DESC").limit(1)
-      if iw.count > 0 && !check_already_win(iw.first) && !@win
-        @win = true
-        @prize = iw.first.contest_periodicity.instant_win_prizes.first
-        
-        PlayticketEvent.create(:points_spent => contest.conversion_rate, :used_at => ctime, :winner => true, 
-                                :user_id => current_user.id, :instantwin_id => iw.first.id)
-
-        @contest_points.update_attribute(:points,@contest_points.points - contest.conversion_rate)
-        
-        #TODO send winner mail
-        #send_winner_email(iw,prize)
+      time_to_win_list = Instantwin.where("contest_periodicity_id = ? AND instantwins.time_to_win<?",cp.id,ctime).order("instantwins.time_to_win DESC").limit(1)
+      if time_to_win_list.count > 0
+        time_to_win = time_to_win_list.first 
+        if !(check_already_win(time_to_win) || @win)
+          @win = true
+          @prize = iw.first.contest_periodicity.instant_win_prizes.first
+          
+          PlayticketEvent.create(:points_spent => contest.conversion_rate, :used_at => ctime, :winner => true, 
+                                  :user_id => current_user.id, :instantwin_id => iw.first.id)
+  
+          @contest_points.update_attribute(:points, @contest_points.points - contest.conversion_rate)
+          
+          # TODO: send winner mail
+          #send_winner_email(iw,prize)
+        end
       end
     end
     return @win
   end
   
-  #TODO MAXIBON
+  # TODO: maxibon
   #
   # check if a prize is already winned
   #
