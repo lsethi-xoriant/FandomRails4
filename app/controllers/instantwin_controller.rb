52017a89505c0367f5287f0747a526e1f3fcdbda
Alessandro Tolomio <alessandrotolomio@MacBook-Pro-di-Alessandro.local>
2014-05-28 08:49:31 +0200

    [MB27] Review instant win, to complete

--- app/controllers/instantwin_controller.rb
+++ app/controllers/instantwin_controller.rb
@@ -2,7 +2,6 @@
 # encoding: utf-8
 
 class InstantwinController < ApplicationController
-  layout "admin"
   DAYS_IN_MONTH = [nil, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
   
   #
@@ -16,80 +15,76 @@ class InstantwinController < ApplicationController
    DAYS_IN_MONTH[month]
   end
 
-  #TODO MAXIBON
   #
   # play a ticket for current user on the contest
   #
   # contest_id - id of the contest
   #
   def play_ticket_mb
-    @ticketended = false
-    @win = false
 
-    risp = Hash.new;
+    win = false
+
     if current_user
-      contest = Contest.find(params[:contest_id])
-      @contest_points = ContestPoint.where("contest_id=? AND user_id = ?",contest.id,current_user.id)   
-      if @contest_points.count > 0 && @contest_points.first.points/contest.conversion_rate > 0
+      contest = Contest.active.find(params[:contest_id])
+      contest_points = ContestPoint.where("contest_id=? AND user_id=?", contest.id, current_user.id)   
+      if contest_points.count > 0 && contest_points.first.points/contest.conversion_rate > 0
 
-        @contest_points = @contest_points.first
+        contest_points = contest_points.first
 
-        time_current = Time.now.in_time_zone("Rome")
-        if !checkWin_mb(time_current,contest)
+        time_current = Time.now.utc
+        unless (win = check_win_mb(time_current, contest, contest_points))
 
           PlayticketEvent.create(:points_spent => contest.conversion_rate, :used_at => time_current, :winner => false, 
                                   :user_id => current_user.id)
 
-          @contest_points.update_attribute(:points,@contest_points.points - contest.conversion_rate)
+          contest_points.update_attribute(:points, contest_points.points - contest.conversion_rate)
 
         end
-      else
-        @ticketended = true
       end
     end
 
-    risp['winner'] = @win
-    risp['ticketended'] = @ticketended
-    risp['prize'] = @prize
+    risp = {
+      'winner' => win
+      'points_updated' = get_current_contest_points current_user.id
+      'prize' = @prize
+    }
+
     respond_to do |format|
       format.json { render :json => risp.to_json }
     end
   end
   
-  #TODO MAXIBON
-  #
-  # check if a ticket is winner
-  #
-  # ctime - timestamp of played ticket
-  # contest - contest for which the ticket is used
-  #
-  def checkWin_mb(ctime,contest)
+  def check_win_mb ctime, contest, contest_points
+    win = false
     contest.contest_periodicities.each do |cp|
-      iw = Instantwin.where("contest_periodicity_id = ? AND instantwins.time_to_win<?",cp.id,ctime).order("instantwins.time_to_win DESC").limit(1)
-      if iw.count > 0 && !check_already_win(iw.first) && !@win
-        @win = true
+      # TODO - I can't win prizes about days before.
+      iw = Instantwin.where("contest_periodicity_id=? AND instantwins.time_to_win<?", cp.id, ctime).order("instantwins.time_to_win DESC").limit(1)
+      if iw.count > 0 && !check_already_win(iw.first) && !win
+        
+        win = true
+
+        # TODO - If possible transform @prize in prize and adjust the code.
         @prize = iw.first.contest_periodicity.instant_win_prizes.first
         
         PlayticketEvent.create(:points_spent => contest.conversion_rate, :used_at => ctime, :winner => true, 
                                 :user_id => current_user.id, :instantwin_id => iw.first.id)
 
-        @contest_points.update_attribute(:points,@contest_points.points - contest.conversion_rate)
+        contest_points.update_attribute(:points, contest_points.points - contest.conversion_rate)
         
-        #TODO send winner mail
-        #send_winner_email(iw,prize)
+        # TODO - send winner mail
+        # send_winner_email(iw,prize)
       end
     end
-    return @win
+    return win
   end
   
-  #TODO MAXIBON
   #
-  # check if a prize is already winned
+  # Check if a prize is already winned
   #
   # iw - instantwin passed
   #
-  def check_already_win(iw)
-    return !iw.playticket_event.nil?
+  def check_already_win iw
+    return iw.playticket_event.present?
   end
 
 end
--- app/helpers/instantwin_helper.rb
+++ app/helpers/instantwin_helper.rb
@@ -7,10 +7,8 @@ module InstantwinHelper
 	# points - points gained by the user
 	#
 	def update_contest_points user_id, points
-		begin
-		  time_current = Time.now.utc 
-		  
-		  Contest.where("start_date<? AND end_date>?", time_current, time_current).each do |ac|
+		begin	  
+		  Contest.active.each do |ac|
 		    contestpoints = ContestPoint.where("user_id=? AND contest_id=?", user_id, ac.id)
 	      if contestpoints.count > 0
 	        contestpoints.first.update_attribute(:points, contestpoints.first.points + points)
@@ -28,9 +26,7 @@ module InstantwinHelper
 	# user_id - id of current user
 	#
 	def get_current_contest_points user_id
-		time_current = Time.now.utc 
-		if (c = Contest.where("start_date<? AND end_date>?", time_current, time_current)).any? &&
-			 (cp = ContestPoint.find_by_user_id_and_contest_id(user_id, c.first.id))
+		if (c = Contest.active.any? && (cp = ContestPoint.find_by_user_id_and_contest_id(user_id, c.first.id))
 		    return cp.points/c.first.conversion_rate
 		else
 			return 0
