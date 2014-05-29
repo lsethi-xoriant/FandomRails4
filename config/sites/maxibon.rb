9c7a522523a4bc216eac2f1731b06ac6e2ab8fb6
Alessandro Tolomio <alessandrotolomio@MacBook-Pro-di-Alessandro.local>
2014-05-29 15:02:53 +0200

    [MB55] Add color about quiz response, right or wrong

--- config/sites/maxibon.rb
+++ config/sites/maxibon.rb
@@ -2,7 +2,7 @@ module Fandom
   class Application < Rails::Application
     register_fandom_site(
       id: 'maxibon', 
-      domains: ['maxibon.fandom.localdomain', 'maxibon.shado.tv', 'maxibon.stage.fandomlab.com', 'maxibon.live.fandomlab.com'],
+      domains: ['maxibon.fandom.localdomain', 'maxibon.shado.tv', 'maxibon.stage.fandomlab.com', 'maxibon.live.fandomlab.com', 'www.maxibon.it', 'maxibon.it'],
       assets_precompile: ['maxibon_application.css', 'maxibon_application.js'],
       environment: { 
         'EMAIL_ADDRESS' => 'noreply@maxibon.it', 
