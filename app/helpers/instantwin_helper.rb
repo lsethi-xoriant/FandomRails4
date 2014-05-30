f30740045ab7f9fd20e713ad57706033405287ba
Diego Dainese <diego@MacBook-Pro-di-Diego.local>
2014-05-30 18:55:01 +0200

    [MB74] further caching; small fix on maxibon redirect

--- app/helpers/instantwin_helper.rb
+++ app/helpers/instantwin_helper.rb
@@ -27,7 +27,7 @@ module InstantwinHelper
 	#
 	def get_current_contest_points user_id
 	  active_contests = cache_short('active_contests') { Contest.active.to_a }
-		if active_contests.any? && (cp = ContestPoint.find_by_user_id_and_contest_id(user_id, c.first.id))
+		if active_contests.any? && (cp = ContestPoint.find_by_user_id_and_contest_id(user_id, active_contests.first.id))
 		   return cp.points/active_contests.first.conversion_rate
 		else
 			return 0
