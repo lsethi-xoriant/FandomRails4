5461674e816635dc8467113e02b0471afd45cad8
Matteo Pozzana <mpozzana@3pstudio.it>
2014-05-30 16:36:02 +0200

    [MB42] Mail for winner clean controller and rewrite template erb for mail

--- app/mailers/system_mailer.rb
+++ app/mailers/system_mailer.rb
@@ -19,18 +19,6 @@ class SystemMailer < ActionMailer::Base
   	@cuser = user
   	@ticket_id = time_to_win.unique_id
   	
-  	if @price.title == "Un ingresso gratuito giornaliero all'Aquafan"
-  	  @prize_won_message = "Hai vinto un ingresso gratuito giornaliero all’ Aquafan di Riccione."
-  	  @message_win_validity = "Potrai utilizzare il tuo ingresso gratuito un qualsiasi giorno compreso fra il XX/XX/2014 e il XX/XX/2014. "
-  	elsif @price.title == "Un pacchetto di 5 biglietti per il Maxiparty con David Guetta"
-  	  @prize_won_message = "Hai vinto un pacchetto di 5 biglietti per te e i tuoi amici per il Maxiparty con David Guetta all’Aquafan di Riccione."
-  	  @message_win_validity = "Potrai utilizzare il tuo ingresso il 3 agosto 2014."
-  	elsif @price.title == "Un pacchetto di 5 biglietti per un evento serale Aquafan"
-  	  @prize_won_message = "Hai vinto un pacchetto di 5 biglietti per un evento serale che si terrà all’Aquafan di Riccione nel mese di agosto.
-Contatta Aquafan all’indirizzo info@aquafan.it o al numero +390541603050 per scoprire di quale evento si tratta."
-      @message_win_validity = "Potrai utilizzare il tuo ingresso gratuito un qualsiasi giorno compreso fra il XX/XX/2014 e il XX/XX/2014."
-  	end
-  	
   	mail(to: user.email, subject: "MAXIBON - PARCO DIVERTIMENTI AQUAFAN 2014 – hai vinto #{ @price.title }")
   end
 
