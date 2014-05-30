653237fd90b0915a0a07004e00a0200edbd96c36
Diego Dainese <diego@MacBook-Pro-di-Diego.local>
2014-05-30 16:00:05 +0200

    [MB2] Some polishing on the maxibon redirect check

--- lib/sites/maxibon/utils.rb
+++ lib/sites/maxibon/utils.rb
@@ -5,7 +5,7 @@ module MaxibonUtils
 
   class Matcher
     def matches?(request)
-      referer_is_facebook = (request.env['HTTP_REFERER']=~/facebook.com|maxibon.it|/).to_f > 0
+      referer_is_facebook = (request.env['HTTP_REFERER']=~/facebook\.com|maxibon\.it/).to_f > 0
       is_mobile = mobile_device? request
       return !referer_is_facebook && !is_mobile      
     end
