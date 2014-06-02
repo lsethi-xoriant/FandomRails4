8b103e227b58b4a98a686f129811119ac3a7fd5c
Alessandro Tolomio <alessandrotolomio@MacBook-Pro-di-Alessandro.local>
2014-06-02 19:17:46 +0200

    [MB91] Temporary landing for sponsored Maxibon song, before live

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,7 @@ class ApplicationController < ActionController::Base
     redirect_to "/"
   end
 
-  before_filter :authenticate_admin, :if => proc {|c| Rails.env == "production" }
+  #before_filter :authenticate_admin, :if => proc {|c| Rails.env == "production" }
 
   def authenticate_admin
     authenticate_or_request_with_http_basic do |username, password|
