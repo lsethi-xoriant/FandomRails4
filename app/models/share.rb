ec982d8fb857ff3e4e2e590e4b251f65a79510ba
Alessandro Tolomio <alessandrotolomio@MacBook-Pro-di-Alessandro.local>
2014-06-03 17:26:08 +0200

    [MB70] Add share custom link column

--- app/models/share.rb
+++ app/models/share.rb
@@ -1,5 +1,5 @@
 class Share < ActiveRecord::Base
-  attr_accessible :share_type, :message, :description, :picture, :cache_share_counter
+  attr_accessible :share_type, :message, :description, :picture, :cache_share_counter, :link
   has_attached_file :picture, :styles => { :large => "600x600", :medium => "234x139>", :thumb => "100x100>" }
   
   has_one :interaction, as: :resource
