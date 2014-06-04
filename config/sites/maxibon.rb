5a460e01806cc33cf7563f2231153f52e976e4f3
Alessandro Tolomio <alessandrotolomio@MacBook-Pro-di-Alessandro.local>
2014-06-04 11:32:11 +0200

    [MB102] Graphics fix

--- config/sites/maxibon.rb
+++ config/sites/maxibon.rb
@@ -13,3 +13,27 @@ module Fandom
   end
 end
 
+=begin
+  module Fandom
+    class Application < Rails::Application
+      register_fandom_site(
+        id: 'maxibon', 
+        domains: ['maxibon.fandom.localdomain', 'maxibon.shado.tv', 'maxibon.stage.fandomlab.com', 'maxibon.live.fandomlab.com', 'www.maxibon.it', 'maxibon.it'],
+        assets_precompile: ['maxibon_application.css', 'maxibon_application.js'],
+        environment: { 
+          'EMAIL_ADDRESS' => 'noreply@maxibon.it', 
+        },
+        
+        desktop_iframe: {
+          forced_except: ['youtube'],
+          allowed_referrers: ['maxibon.it', 'facebook.com', 'youtube.com'] 
+        },
+
+        logged_url: 'https://www.facebook.com/MaxibonMaxiconoItalia/app_597403706967732'
+
+        #enable_x_frame_options_header: false
+        #force_facebook_tab: 'https://www.facebook.com/MaxibonMaxiconoItalia/app_597403706967732' 
+      )
+    end
+  end
+=end
