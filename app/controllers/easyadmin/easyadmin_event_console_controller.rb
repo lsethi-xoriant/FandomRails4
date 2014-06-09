ecbaaa441022ee77220d55c7bd99979c4b2be0e5
Matteo Pozzana <mpozzana@3pstudio.it>
2014-06-09 09:58:20 +0200

    [106] Added documentation to easyadmin_event_console_controller

--- app/controllers/easyadmin/easyadmin_event_console_controller.rb
+++ app/controllers/easyadmin/easyadmin_event_console_controller.rb
@@ -4,24 +4,32 @@ class Easyadmin::EasyadminEventConsoleController < ApplicationController
   include TableHelper
 
   layout "admin"
-    
+  
+  # Constant that describe the filter available
   FIELD_DESCS = { 
     :date => FieldDesc.new({ :name => "Data", :id => "date", :model => "userinteraction", :column_name => "updated_at"}),
     :user => FieldDesc.new({ :name => "Utente", :id => "user", :model => "user", :column_name => "email"}),
     :interaction => FieldDesc.new({ :name => "Interazione", :id => "interaction", :model => "interaction", :column_name => "resource_type" })
   }
   
+  # json version of fields description
   FIELD_DESCS_JSON = (FIELD_DESCS.map { |id, obj| [id, obj.attributes] }).to_json
   
+  # Return the fields description Hash
   def get_fields
     FIELD_DESCS
   end
   
+  # Called at first load initialize the filter interaction with the interaction type active
   def index
     @interaction_type_options = Interaction.uniq.pluck(:resource_type)
     @fields = FIELD_DESCS_JSON
   end
   
+  # Give back the events query result depending on conditions passes as parameter
+  #
+  # offset - current page results to load
+  # limit  - number of results per page
   def get_results(offset, limit)
     if params[:conditions].blank?
       events = Userinteraction.limit(limit).offset(offset).order("updated_at ASC")
@@ -32,6 +40,9 @@ class Easyadmin::EasyadminEventConsoleController < ApplicationController
     return events
   end
   
+  # construct the query to retrive events depending on filter params passed as parameter
+  #
+  # params - array of filter conidtions
   def build_query(params)
     query = Userinteraction.includes(:user).includes(:interaction).includes(:answer)
     fields = get_fields()
@@ -44,11 +55,13 @@ class Easyadmin::EasyadminEventConsoleController < ApplicationController
       else
         query = query.where( get_active_record_expression(fields[field_id]['column_name'], operator, fields[field_id]['model']), operand )
       end
-      
     end
     return query  
   end
-
+  
+  # convert an element of the resultset into a hash easy to converto in json response
+  #
+  # e - resultset element
   def element_to_result(e)
     result = Hash.new
     result['date'] = e.updated_at.strftime("%d-%m-%Y %H:%M")
@@ -56,6 +69,5 @@ class Easyadmin::EasyadminEventConsoleController < ApplicationController
     result['interaction'] = e.interaction.resource_type
     result
   end
-
   
 end
