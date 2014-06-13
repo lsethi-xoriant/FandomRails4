431137c7b873cb7d0c096f933345613f6a343def
Diego Dainese <diego@MacBook-Pro-di-Diego.local>
2014-06-13 10:35:57 +0200

    [156] Refactoring of the session controller, added an automatic login into FandomPlay

--- app/controllers/rss_controller.rb
+++ app/controllers/rss_controller.rb
@@ -1,18 +1,17 @@
 class RssController < ApplicationController
-	
-  	def property_rss
-  		@current_prop = Property.find(params[:property_id])
-	  	@calltoaction_list = Calltoaction.active.where("property_id=?", @current_prop.id).order("activated_at DESC").limit(10)
-	  	respond_to do |format|
-	    	format.rss { render :layout => false }
-	  	end
-	end
+  def property_rss
+    @current_prop = Property.find(params[:property_id])
+    @calltoaction_list = Calltoaction.active.where("property_id=?", @current_prop.id).order("activated_at DESC").limit(10)
+    respond_to do |format|
+      format.rss { render :layout => false }
+    end
+  end
 
-	def global_rss
-	  	@calltoaction_list = Calltoaction.active.order("activated_at DESC").limit(10)
-	  	respond_to do |format|
-	    	format.rss { render :layout => false }
-	  	end
-	end
+  def global_rss
+    @calltoaction_list = Calltoaction.active.order("activated_at DESC").limit(10)
+    respond_to do |format|
+      format.rss { render :layout => false }
+    end
+  end
 
 end
