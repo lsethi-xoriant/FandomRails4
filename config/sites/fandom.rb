1a7e7ffb8a78b6d82bede3fa3b8205bbc1175052
Diego Dainese <diego@MacBook-Pro-di-Diego.local>
2014-06-25 14:50:42 +0200

    [136] Added a new subdomain for fandomlab.com

--- config/sites/fandom.rb
+++ config/sites/fandom.rb
@@ -3,7 +3,7 @@ module Fandom
     register_fandom_site(
       id: 'fandom',
       title: 'Fandom',
-      domains: ['fandom.shado.tv', 'fandom.localdomain', 'stage.fandomlab.com', 'live.fandomlab.com'],
+      domains: ['fandom.shado.tv', 'fandom.localdomain', 'stage.fandomlab.com', 'live.fandomlab.com', 'demo.fandomlab.com'],
       twitter_integration: true,
       environment: { 'EMAIL_ADDRESS' => 'noreply@shado.tv' }
     )
