2508b0f39cf71e9772036fe256b6a801ad8db64e
Matteo Pozzana <mpozzana@3pstudio.it>
2014-06-26 11:41:33 +0200

    [161] Added rewarding rules editor in easyadmin

---
+++ app/controllers/easyadmin/easyadmin_rewarding_rules_controller.rb
@@ -0,0 +1,35 @@
+class Easyadmin::EasyadminRewardingRulesController < ApplicationController
+  include EasyadminHelper
+  include RewardingSystemHelper
+
+  layout "admin"
+
+  def index
+    #@rules = Setting.find_by_key(REWARDING_RULE_SETTINGS_KEY)
+    @saved = true
+    rules = Setting.find_by_key("regole")
+    if !rules
+      rules = Setting.create(:key => "regole", :value => "")
+      #@rules = Setting.create(:key => REWARDING_RULE_SETTINGS_KEY, :value => "")
+    end
+    @rules_value = rules.value
+  end
+  
+  def save
+    errors = check_rules(params[:rules])
+    if errors.any?
+      flash[:error] = errors
+      @saved = false
+      @rules_value = params[:rules]
+      render template: "/easyadmin/easyadmin_rewarding_rules/index"
+    else
+      rules = Setting.find_by_key(params[:key])
+      rules.update_attribute(:value, params[:rules])
+      @saved = true
+      @rules_value = rules.value
+      flash[:notice] = "Regole aggiornate correttamente"
+      render template: "/easyadmin/easyadmin_rewarding_rules/index"
+    end
+  end
+
+end
