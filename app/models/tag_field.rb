643fc3cde8e478be0492a3bb2a34a908c3e86b73
Matteo Pozzana <mpozzana@3pstudio.it>
2014-06-27 15:40:03 +0200

    [166] Updated tag model with new columns

--- app/models/tag_field.rb
+++ app/models/tag_field.rb
@@ -1,5 +1,5 @@
 class TagField < ActiveRecord::Base
-  attr_accessible :name, :type, :value, :tag_id
+  attr_accessible :name, :type, :value, :tag_id, :description, :locked
 
   validates_presence_of :name
   
