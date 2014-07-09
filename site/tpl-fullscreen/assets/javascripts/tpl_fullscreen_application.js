f1b54983f632f80e58a0e7f7fcbab24b40895e4a
Francesco Fraioli <fraiolinotes@gmail.com>
2014-07-09 18:40:35 +0200

    [202] Fullscreen template update

---
+++ site/tpl-fullscreen/assets/javascripts/tpl_fullscreen_application.js
@@ -0,0 +1,16 @@
+//= require jquery
+//= require jquery_ujs
+//= require jquery-ui-1.10.4.custom
+//= require angular
+//= require angular-resource
+//= require angular-route
+//= require angularjs/ng-table
+//= require angularjs/fandom
+//= require angularjs/notice
+//= require angularjs/calltoaction
+//= require angularjs/comment
+//= require angularjs/stream_calltoaction
+//= require bootstrap
+//= require bootstrap-notify
+//= require bootstrap-fileupload
+//= require jquery.jplayer
