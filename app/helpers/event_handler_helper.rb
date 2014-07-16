6eadb9ebacc31247433dddc21e84242c16a9397c
Alessandro Tolomio <alessandrotolomio@MacBook-Pro-di-Alessandro.local>
2014-07-16 09:20:01 +0200

    [164] Add backtrace line parsing in event handler logger

--- app/helpers/event_handler_helper.rb
+++ app/helpers/event_handler_helper.rb
@@ -1,8 +1,13 @@
 module EventHandlerHelper
 
-  # TODO: to be implemented
-  def log_event(msg, data)
-    logger.info("event: #{msg}; data: #{data}")
+  def log_info_event(msg, data)
+    caller_data = caller[0].match(/^(.+?):(\d+)(|:in `(.+)')$/);
+
+    file_name = caller_data[1]
+    line_number = caller_data[2]
+    method_name = caller_data[4]
+
+    logger.info("event: #{msg}; data: #{data}; line_number: #{line_number}; file_name: #{file_name}; method_name: #{method_name}")
   end
 
   def log_system_event()
