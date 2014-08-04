76c953a6d20ce5c10efa08f4b0a624b81d8424ef
Diego Dainese <ddainese@shado.tv>
2014-08-04 18:22:10 +0200

    [closes #4] script to get the name of an EC2 instance

---
+++ bin/get_aws_name.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#
+# This script output the value of the tag "Name" of the currently running instance.
+# It requires awscli and cloud-utils pacakges.
+# In case some command fails, the hostname is printed instead
+#
+
+name=$(echo print $(aws ec2 describe-tags --filters "Name=resource-id,Values=$(ec2metadata --instance-id)") '["Tags"][0]["Value"]' | python)
+
+if [ -z $name ]; then
+	hostname
+else
+	echo $name
+fi
