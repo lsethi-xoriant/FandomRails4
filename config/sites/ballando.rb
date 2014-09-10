a439e21e52b4fe2a168209ce41848cb65623c7dd
Alessandro Tolomio <alessandrotolomio@MacBook-Pro-di-Alessandro.local>
2014-09-10 16:00:13 +0200

    [refs #161] Added versus without image for ballando tenant

--- config/sites/ballando.rb
+++ config/sites/ballando.rb
@@ -5,6 +5,7 @@ module Fandom
       title: 'Ballando con le stelle',
       domains: ['ballando.fandom.localdomain', 'ballando.shado.tv', 'ballando.stage.fandomlab.com', 'ballando.live.fandomlab.com', 'ballando.dev.fandomlab.com'],
       assets_precompile: ['ballando_application.css', 'ballando_application.js'],
+      share_db: 'fandom',
       environment: { 
         'EMAIL_ADDRESS' => 'noreply@ballando.it', 
       }
