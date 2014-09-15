bfc58a463db1ae90315024aa0fec04823dbadb27
Diego Dainese <ddainese@shado.tv>
2014-09-15 09:45:01 +0200

    [refs #177] added another domain to ballando

--- config/sites/ballando.rb
+++ config/sites/ballando.rb
@@ -3,7 +3,7 @@ module Fandom
     register_fandom_site(
       id: 'ballando', 
       title: 'Ballando con le stelle',
-      domains: ['ballando.fandom.localdomain', 'ballando.shado.tv', 'ballando.stage.fandomlab.com', 'ballando.live.fandomlab.com', 'ballando.dev.fandomlab.com'],
+      domains: ['ballando.fandom.localdomain', 'ballando.shado.tv', 'ballando.stage.fandomlab.com', 'ballando.live.fandomlab.com', 'ballando.dev.fandomlab.com', 'www.stage.community.ballando.rai.it'],
       assets_precompile: ['ballando_application.css', 'ballando_application.js'],
       environment: { 
         'EMAIL_ADDRESS' => 'noreply@ballando.it', 
