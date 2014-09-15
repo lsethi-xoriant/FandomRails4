0ce572f54dad46f20bb819dc3c234f9bfa564c02
Matteo Pozzana <mpozzana@shado.tv>
2014-09-15 15:52:55 +0200

    [closes #145] Closed iframe check with logged, not logged, cap reached and available check.

--- app/controllers/sites/ballando/iframe_check_controller.rb
+++ app/controllers/sites/ballando/iframe_check_controller.rb
@@ -1,5 +1,7 @@
 class Sites::Ballando::IframeCheckController < ApplicationController
   
+  layout "application_light"
+  
   def show
     render template: "/iframe_check/show"
   end
@@ -8,9 +10,11 @@ class Sites::Ballando::IframeCheckController < ApplicationController
     referrer = params[:referrer]
     cta = CallToAction.find_by_name("check_iframe")
     check_interaction = cta.interactions.first
-    if can_do_check(check_interaction, referrer)
+    if !current_user
+      template = render_to_string "/iframe_check/_not_logged_check", locals: { interaction: check_interaction }, layout: false, formats: :html
+    elsif can_do_check(check_interaction, referrer)
       template = render_to_string "/iframe_check/_check_to_do", locals: { interaction: check_interaction }, layout: false, formats: :html
-    elsif reached_cap(check_interaction)
+    elsif reached_cap
       template = render_to_string "/iframe_check/_cap_reached", locals: { interaction: check_interaction }, layout: false, formats: :html
     else
       template = render_to_string "/iframe_check/_check_already_done", locals: { interaction: check_interaction }, layout: false, formats: :html
@@ -21,8 +25,8 @@ class Sites::Ballando::IframeCheckController < ApplicationController
   end
   
   def can_do_check(interaction, referrer)
-    user_interaction = UserInteraction.where("interaction_id = ? AND user_id = ?", check_interaction.id, current_user.id).first
-    if user_interaction.nil? || ( !reached_cap(interaction) && !has_already_checked(interaction, referrer) ) 
+    user_interaction = UserInteraction.where("interaction_id = ? AND user_id = ?", interaction.id, current_user.id).first
+    if user_interaction.nil? || ( !reached_cap && !has_already_checked(interaction, referrer) ) 
       true
     else
       false
@@ -33,13 +37,16 @@ class Sites::Ballando::IframeCheckController < ApplicationController
     referrer = params[:referrer]
     cta = CallToAction.find_by_name("check_iframe")
     check_interaction = cta.interactions.first
+    aux = {}
     if UserInteraction.where("interaction_id = ? AND user_id = ?", check_interaction.id, current_user.id).count > 0
       user_interaction = UserInteraction.where("interaction_id = ? AND user_id = ?", check_interaction.id, current_user.id).first
-      aux = user_interaction.aux+",#{referrer}"
+      aux = JSON.parse(user_interaction.aux)
+      aux['referrer_list'] = aux['referrer_list'] + ",#{referrer}"
     else
-      aux = "{referrer_list: '#{referrer}'}"
+      aux['referrer_list'] = "#{referrer}"
+      #aux = "{referrer_list: '#{referrer}'}"
     end
-    update_check_counter(interaction, aux)
+    update_check_counter(check_interaction, aux.to_json)
     template = render_to_string "/iframe_check/_check_already_done", locals: { interaction: check_interaction }, layout: false, formats: :html 
     respond_to do |format|
        format.json { render :json => template.to_json }
@@ -47,10 +54,10 @@ class Sites::Ballando::IframeCheckController < ApplicationController
   end
   
   def update_check_counter(interaction, aux)
-    UserInteraction.create_or_update_interaction(current_user.id, interaction.id, nil, nil, aux)
+    create_or_update_interaction(current_user.id, interaction.id, nil, nil, aux)
   end
   
-  def reached_cap(interaction)
+  def reached_cap
     check_counter = UserCounter.get_by_user(current_user)["DAILY"]["ALL_CHECK"]
     if check_counter.nil? || check_counter < DAILY_CHECK_CAP
       false
@@ -69,7 +76,8 @@ class Sites::Ballando::IframeCheckController < ApplicationController
   end
   
   def is_referrer_present(referrer, aux)
-    aux.split(",").include(referrer)
+    aux = JSON.parse(aux)
+    aux['referrer_list'].split(",").include?(referrer)
   end
   
 end
---
+++ site/ballando/assets/javascripts/angularjs/ballando_iframe_calltoaction.js
@@ -0,0 +1,28 @@
+var ballandoIframeCalltoactionModule = angular.module('BallandoIframeCalltoactionModule', ['ngRoute']);
+
+BallandoIframeCalltoactionCtrl.$inject = ['$scope', '$window', '$http'];
+ballandoIframeCalltoactionModule.controller('BallandoIframeCalltoactionCtrl', BallandoIframeCalltoactionCtrl);
+
+function BallandoIframeCalltoactionCtrl($scope, $window, $http, $timeout, $interval) {
+
+  $window.onEnterInteraction = function(interaction_id, answer_id) {
+    if(answer_id) {
+      $("#answer-" + answer_id).find(".interaction-baloon .baloon.unchosen img").removeClass("hidden");
+      $("#answer-" + answer_id).find(".interaction-baloon .baloon.unchosen").removeClass("square");
+    } else {
+      $("#iframe-interaction").find(".interaction-baloon .baloon.unchosen img").removeClass("hidden");
+      $("#iframe-interaction").find(".interaction-baloon .baloon.unchosen").removeClass("square");
+    }
+  };
+
+  $window.onLeaveInteraction = function(interaction_id, answer_id) {
+    if(answer_id) {
+      $("#answer-" + answer_id).find(".interaction-baloon .baloon.unchosen img").addClass("hidden");
+      $("#answer-" + answer_id).find(".interaction-baloon .baloon.unchosen").addClass("square");
+    } else {
+      $("#iframe-interaction").find(".interaction-baloon .baloon.unchosen img").addClass("hidden");
+      $("#iframe-interaction").find(".interaction-baloon .baloon.unchosen").addClass("square");
+    }
+  };
+
+}
