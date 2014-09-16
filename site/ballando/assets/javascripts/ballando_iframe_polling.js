dd5890664d1c5ca80345acedf287e8b5f3f1ff9a
Diego Dainese <ddainese@shado.tv>
2014-09-16 10:49:02 +0200

    [refs #157] new code to set document.domain for ballando

--- site/ballando/assets/javascripts/ballando_iframe_polling.js
+++ site/ballando/assets/javascripts/ballando_iframe_polling.js
@@ -1,4 +1,17 @@
-document.domain = "shado.tv";
+function setBallandoDomain() {
+	String.prototype.endsWith = function(suffix) {
+	    return this.indexOf(suffix, this.length - suffix.length) !== -1;
+	};
+	var index;
+	var domains = ["fandomlab.com", "ballando.rai.it", "shado.tv"];
+	for (index = 0; index < domains.length; ++index) {
+		if (window.location.host.endsWith(domains[index])) {
+			console.log("domain set to: " + domains[index]);
+	    	document.domain = domains[index];
+		}
+	}
+}
+setBallandoDomain();
 
 function checkDocumentHeight(){
   var lastHeight = $("body").innerHeight(), newHeight, timer;
