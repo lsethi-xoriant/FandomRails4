ae5fa5f49b06fdcf2cf486ed7ed66de08e8599d4
Alessandro Tolomio <alessandrotolomio@MacBook-Pro-di-Alessandro.local>
2014-09-17 10:52:05 +0200

    [refs #143] Added provider login

--- config/sites/ballando.rb
+++ config/sites/ballando.rb
@@ -5,6 +5,7 @@ module Fandom
       title: 'Ballando con le stelle',
       domains: ['ballando.localdomain', 'ballando.fandom.localdomain', 'ballando.shado.tv', 'ballando.stage.fandomlab.com', 'ballando.live.fandomlab.com', 'ballando.dev.fandomlab.com', 'www.stage.community.ballando.rai.it'],
       assets_precompile: ['ballando_application.css', 'ballando_application.js', 'ballando_application_light.js'],
+      share_db: 'fandom',
       environment: { 
         'EMAIL_ADDRESS' => 'noreply@ballando.it', 
       }
