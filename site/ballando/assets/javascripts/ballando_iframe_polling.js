14ea5b15d76cae796d0de6d1c83979dc02725066
Alessandro Tolomio <alessandrotolomio@MacBook-Pro-di-Alessandro.local>
2014-09-18 16:33:19 +0200

    [closes #161] Updated polling, in all pages

---
+++ site/ballando/assets/javascripts/ballando_iframe_polling.js
@@ -0,0 +1,13 @@
+function checkDocumentHeight(myIframeId) {
+  var lastHeight = 0;
+  (function run(){
+    newHeight = $("body").innerHeight();
+    if(lastHeight != newHeight) {
+      window.parent.containerHeight(newHeight, myIframeId);
+    }
+    lastHeight = newHeight;
+    timer = setTimeout(run, 300);
+  })();
+}
+
+checkDocumentHeight("fandom");
