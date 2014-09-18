48ef98590d560328fa7fe220e8c7a624eadef0a4
Alessandro Tolomio <alessandrotolomio@MacBook-Pro-di-Alessandro.local>
2014-09-18 18:20:33 +0200

    [CQ] Removed cache in periodicity_helper, this method is called from method that has not request

--- app/helpers/periodicity_helper.rb
+++ app/helpers/periodicity_helper.rb
@@ -6,14 +6,14 @@ require 'fandom_utils'
 module PeriodicityHelper
   
   def get_current_periodicities
-    cache_short("current_periodicities") do
+    #cache_short("current_periodicities") do
       period_list = Hash.new
       periods = Period.where("start_datetime < ? AND end_datetime > ?", Time.now, Time.now)
       periods.each do |period|
         period_list[period.kind] = period
       end
       period_list
-    end
+    #end
   end
   
   def get_period_by_kind(periodicity_kind)
