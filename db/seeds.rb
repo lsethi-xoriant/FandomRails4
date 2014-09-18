997d473f13a1cc124ba180c1f5aa712b8ffccf6a
Diego Dainese <ddainese@shado.tv>
2014-09-18 17:05:24 +0200

    [CQ] default seeds cleaned up. Added some log to dalli-elasticache

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,3 @@
-## CREAZIONE UTENTI AMMINISTRATORI DI DEFAULT ##
-
 User.create(email: "atolomio@shado.tv", first_name: "Martino", last_name: "Russo", 
             privacy: true, password: "shado00", password_confirmation: "shado00", role: "admin")
 
@@ -19,8 +17,6 @@ User.create(email: "anonymous@shado.tv", first_name: "Anonymous", last_name: "An
             privacy: true, password: "shado00", password_confirmation: "shado00")
 
 
-## DEFINIZIONE TAG ##
-
 Tag.create(name: "basic", description: "serve per nascondere i badge e livelli dalla pagina catalogo premi", locked: true)
 
 Tag.create(name: "top", description: "serve per posizionare gli elementi in alto e in evidenza all'interno della pagina catalogo premi", locked: true)
@@ -34,61 +30,9 @@ Tag.create(name: "miniformat", description: "serve per poter visualizzare l'icon
 Tag.create(name: "gallery", description: "serve per poter abilitare la gallery", locked: true)
 
 
-## DEFINIZIONE REGOLE DI REWARD ##
-
 Setting.create(key: "rewarding.rules", value: <<-EOF
-
-rule "TRIVIA_ANSWER",
-  interactions: { types: ["Trivia"] },
-  rewards: [{ "point" => 1,  "credit" => 1}] 
-
-rule "TRIVIA_CORRECT",
-  interactions: { types: ["Trivia"] },
-  rewards: [{ "point" => 3, "credit" => 2}] { 
-    correct_answer
-  }
-
-rule "VERSUS_ANSWER",
-  interactions: { types: ["Versus"] },
-  rewards: [{ "point" => 1, "credit" => 1 }] 
-
-rule "CHECK_ANSWER",
-  interactions: { types: ["Check"] },
-  rewards: [{ "point" => 1, "credit" => 1 }]
-
-rule "SHARE",
-  interactions: { types: ["Share"] },
-  rewards: [{ "point" => 1, "credit" => 1 }] 
-
-rule "LIKE",
-  interactions: { types: ["Like"] },
-  rewards: [{ "point" => 1, "credit" => 1 }]
-
-rule "DOWNLOAD",
-  interactions: { types: ["Download"] },
-  rewards: [{ "point" => 1, "credit" => 1}]
-
 EOF
 )
 
-## DEFINIZIONE REWARD di tipo BADGE ##
-
 Reward.create(name: "point", title: "point", countable: true, numeric_display: true)
 
-Reward.create(name: "credit", title: "credit", spendable: true, countable: true, numeric_display: true)
-
-Reward.create(name: "trivia1", title: "trivia1", short_description: "Per ottenere questo badge è sufficiente rispondere ad un trivia", long_description: "Per ottenere questo badge è sufficiente rispondere ad un trivia")
-
-Reward.create(name: "trivia3", title: "trivia3", short_description: "Per ottenere questo badge è sufficiente rispondere a tre trivia", long_description: "Per ottenere questo badge è sufficiente rispondere a tre trivia")
-
-Reward.create(name: "versus1", title: "versus1", short_description: "Per ottenere questo badge è sufficiente rispondere a un versus", long_description: "Per ottenere questo badge è sufficiente rispondere a un versus")
-
-Reward.create(name: "versus3", title: "versus3", short_description: "Per ottenere questo badge è sufficiente rispondere a tre versus", long_description: "Per ottenere questo badge è sufficiente rispondere a tre versus")
-
-Reward.create(name: "share1", title: "share1", short_description: "Per ottenere questo badge è sufficiente condividere un contenuto", long_description: "Per ottenere questo badge è sufficiente condividere un contenuto")
-
-Reward.create(name: "share3", title: "share3", short_description: "Per ottenere questo badge è sufficiente condividere tre contenuti", long_description: "Per ottenere questo badge è sufficiente condividere tre contenuti")
-
-Reward.create(name: "download1", title: "download1", short_description: "Per ottenere questo badge è sufficiente effettuare un download di un contenuto", long_description: "Per ottenere questo badge è ssufficiente effettuare un download di un contenuto")
-
-Reward.create(name: "download3", title: "download3", short_description: "Per ottenere questo badge è sufficiente effettuare un download di tre contenuto", long_description: "Per ottenere questo badge è sufficiente effettuare un download di tre contenuti")
