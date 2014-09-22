5e738f3b2e1ed58764ceb88c53c7cb4341bd73ac
Alessandro Tolomio <alessandrotolomio@MacBook-Pro-di-Alessandro.local>
2014-09-22 10:13:05 +0200

    [CQ] Nil var with user not logged fixed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -237,8 +237,13 @@ module ApplicationHelper
 
     reward_status_images = Array.new 
 
-    user_interaction = interaction.user_interactions.find_by_user_id(current_user.id)
-    if current_user && user_interaction
+    if current_user
+      user_interaction = interaction.user_interactions.find_by_user_id(current_user.id)
+    else
+      user_interaction = nil
+    end
+
+    if user_interaction
       win_reward_count = JSON.parse(user_interaction.outcome)["win"]["attributes"]["reward_name_to_counter"].fetch(reward_name, 0)
       correct_answer_outcome = JSON.parse(user_interaction.outcome)["correct_answer"]
       correct_answer_reward_count = correct_answer_outcome ? correct_answer_outcome["attributes"]["reward_name_to_counter"].fetch(reward_name, 0) : 0
