3c41609c9e59bf3aa823106b4fb97d6aa6439d30
Alessandro Tolomio <alessandrotolomio@MacBook-Pro-di-Alessandro.local>
2014-09-22 16:02:57 +0200

    [CQ] Added mobile/tablet detect

--- lib/fandom_utils.rb
+++ lib/fandom_utils.rb
@@ -91,7 +91,7 @@ module FandomUtils
   end
 
   # Returns true if the request comes from a mobile device.
-  def request_is_from_mobile_device?(request)
+  def request_is_from_phone_mobile_device?(request)
     iphone = request_is_from_iphone_device?(request)
     ipad = request.user_agent =~ /iPad/ 
     mobile = request.user_agent =~ /Mobile/
@@ -99,6 +99,16 @@ module FandomUtils
 
     # Mobile and Android identifica il MOBILE di tipo Android, altrimenti con solo Android abbiamo il TABLET.
     return ((iphone && !ipad) || (mobile && android))
+  end
+
+  def request_is_from_mobile_device?(request)
+    iphone = request_is_from_iphone_device?(request)
+    ipad = request.user_agent =~ /iPad/ 
+    mobile = request.user_agent =~ /Mobile/
+    android = request.user_agent =~ /Android/  
+
+    # Mobile and Android identifica il MOBILE di tipo Android, altrimenti con solo Android abbiamo il TABLET.
+    return ((iphone || ipad) || android)
   end
 
   def request_is_from_iphone_device?(request)
