17c4389f8bd0d2bc6972c889f0665c53e4811e91
Diego Dainese <ddainese@shado.tv>
2014-09-23 12:08:00 +0200

    [refs #218] small refactoring of ballando profile widget

--- app/controllers/sites/ballando/iframe_profile_controller.rb
+++ app/controllers/sites/ballando/iframe_profile_controller.rb
@@ -1,7 +1,11 @@
 class Sites::Ballando::IframeProfileController < ApplicationController
+  include NoticeHelper
+  
   def show
-    @notices_count = current_user ? Notice.where("user_id = ? AND viewed = ?", current_user.id, false).count : 0
+    @notices_count = get_unread_notifications_count
+    @profile_url = Rails.configuration.deploy_settings["sites"]["ballando"]["profile_url"]
     render template: "/iframe_profile/show"
   end
+  
 end
 
