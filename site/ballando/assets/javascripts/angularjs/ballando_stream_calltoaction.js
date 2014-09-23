b7fdfa1193891d5f68a77faded6cfe394e8d25bf
Alessandro Tolomio <alessandrotolomio@MacBook-Pro-di-Alessandro.local>
2014-09-23 15:27:28 +0200

    [refs #213] Update close and open calltoaction feature, if I open an old calltoaction done, countdown is not showed

--- site/ballando/assets/javascripts/angularjs/ballando_stream_calltoaction.js
+++ site/ballando/assets/javascripts/angularjs/ballando_stream_calltoaction.js
@@ -29,21 +29,35 @@ function BallandoStreamCalltoactionCtrl($scope, $window, $http, $timeout, $inter
     $(".media-iframe").html("");
     $(".home-undervideo-calltoaction").html("");
 
-    $http.post("/generate_cover_for_calltoaction", { calltoaction_id: calltoaction_id })
-      .success(function(data) {
-        $("#calltoaction-" + calltoaction_id + "-cover").addClass("hidden");
-        $("#home-undervideo-calltoaction-" + calltoaction_id).html(data.render_calltoaction_cover);
+    $scope.interactions_showed[calltoaction_id] = [];
 
+    $http.post("/generate_cover_for_calltoaction", { calltoaction_id: calltoaction_id, interactions_showed: $scope.interactions_showed[calltoaction_id] })
+      .success(function(data) {
+
+        $("#calltoaction-" + calltoaction_id + "-cover").addClass("hidden");
+        
         if(calltoaction_media_type == "iframe") {
           $("#iframe-calltoaction-" + calltoaction_id).html($scope.video_players[calltoaction_id]);
-
           adjustIPhoneIframes();
 
-          $("#iframe-calltoaction-" + calltoaction_id + " iframe").load(function() {
-            appendAndStartCountdown(calltoaction_id); 
-          });
+          if(data.calltoaction_completed) {
+            mountNextInteractionFromRequest(calltoaction_id, data.next_interaction);
+          } else {
+            $("#home-undervideo-calltoaction-" + calltoaction_id).html(data.render_calltoaction_cover);
+            $("#iframe-calltoaction-" + calltoaction_id + " iframe").load(function() {
+              appendAndStartCountdown(calltoaction_id); 
+            });
+          }
+
         } else {
-          appendAndStartCountdown(calltoaction_id); 
+
+          if(data.calltoaction_completed) {
+            mountNextInteractionFromRequest(calltoaction_id, data.next_interaction);
+          } else {
+            $("#home-undervideo-calltoaction-" + calltoaction_id).html(data.render_calltoaction_cover);
+            appendAndStartCountdown(calltoaction_id); 
+          }
+
         }
       }).error(function() {
         // ERROR.
@@ -54,7 +68,7 @@ function BallandoStreamCalltoactionCtrl($scope, $window, $http, $timeout, $inter
   $window.appendAndStartCountdown = function(calltoaction_id) {
     $("#calltoaction-" + calltoaction_id + "-countdown").prepend("<div class=\"wrapper hidden-xs\"><div class=\"pie spinner\"></div><div class=\"pie filler\"></div><div class=\"mask\"></div></div>");
     showCallToActionCountdown(calltoaction_id, COUNTDOWN_TIME);
-  }
+  };
 
   $window.showCallToActionCountdown = function(calltoaction_id, time) {
     if(time > 0) {
@@ -74,23 +88,25 @@ function BallandoStreamCalltoactionCtrl($scope, $window, $http, $timeout, $inter
   $window.nextInteraction = function(calltoaction_id) {
     $http.post("/next_interaction", { interactions_showed: $scope.interactions_showed[calltoaction_id], calltoaction_id: calltoaction_id })
       .success(function(data) {
-
-        $("#home-undervideo-calltoaction-" + calltoaction_id).html(data.render_interaction_str);
-        if(data.interaction_id) {
-          if($scope.interactions_showed[calltoaction_id]) {
-            $scope.interactions_showed[calltoaction_id].push(data.interaction_id);
-          } else {
-            $scope.interactions_showed[calltoaction_id] = [data.interaction_id];
-          }
-        }
-
-        if(data.next_quiz_interaction || $scope.interactions_showed[calltoaction_id].length > 1) {
-          $("#interaction-" + data.interaction_id + "-next").removeClass("hidden");
-        }
-
+        mountNextInteractionFromRequest(calltoaction_id, data);
       }).error(function() {
         // ERROR.
       });
+  };
+
+  $window.mountNextInteractionFromRequest = function(calltoaction_id, data) {
+    $("#home-undervideo-calltoaction-" + calltoaction_id).html(data.render_interaction_str);
+    if(data.interaction_id) {
+      if($scope.interactions_showed[calltoaction_id]) {
+        $scope.interactions_showed[calltoaction_id].push(data.interaction_id);
+      } else {
+        $scope.interactions_showed[calltoaction_id] = [data.interaction_id];
+      }
+    }
+
+    if(data.next_quiz_interaction || $scope.interactions_showed[calltoaction_id].length > 1) {
+      $("#interaction-" + data.interaction_id + "-next").removeClass("hidden");
+    }
   };
 
   $window.userAnswerInAlwaysVisibleInteraction = function(interaction_id, data) {
