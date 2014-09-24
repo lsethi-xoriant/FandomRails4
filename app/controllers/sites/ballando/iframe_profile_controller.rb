c2e7dc54e8b82f3a011992311be783055ad60fc0
Alessandro Tolomio <alessandrotolomio@MacBook-Pro-di-Alessandro.local>
2014-09-24 10:57:21 +0200

    [closes #206] Fixed profile widget button

--- app/controllers/sites/ballando/iframe_profile_controller.rb
+++ app/controllers/sites/ballando/iframe_profile_controller.rb
@@ -1,6 +1,7 @@
 class Sites::Ballando::IframeProfileController < ApplicationController
   def show
-    @notices_count = current_user ? Notice.where("user_id = ? AND viewed = ?", current_user.id, false).count : 0
+    @notices_count = 0 #current_user ? Notice.where("user_id = ? AND viewed = ?", current_user.id, false).count : 0
+    @profile_url = Rails.configuration.deploy_settings["sites"]["ballando"]["profile_url"]
     render template: "/iframe_profile/show"
   end
 end
