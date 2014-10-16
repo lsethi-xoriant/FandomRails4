97920649ead39f08a0728a98f68cb29319552878
Matteo Pozzana <mpozzana@shado.tv>
2014-10-16 11:11:20 +0200

    [closes #357] Fixed bug in show trirank

--- app/assets/javascripts/angularjs/ranking.js
+++ app/assets/javascripts/angularjs/ranking.js
@@ -17,11 +17,16 @@ function RankingCtrl($scope, $window, $resource, $sce) {
 	$scope.init = function(rankings, user) {
 		$scope.rankings = rankings;
 		$scope.user = user;
+		user_off = rankings.general_user_position[user.id] % 10;
+		if(user_off == 0){
+			$scope.user_offset = 9;
+		}else{
+			$scope.user_offset = user_off - 1;
+		}
 	};
 	
 	$scope.prev_page = function(current_page, rank){
 		page = parseInt(current_page) - 1;
-		console.log("PAGE INSIDE PREV " + page);
 		$scope.get_rank_page(page, rank);
 	};
 	
@@ -32,7 +37,7 @@ function RankingCtrl($scope, $window, $resource, $sce) {
 	
 	$scope.get_rank_page = function(page, rank){
 		if(page >= 1 && page <= rank.number_of_pages){
-			Api.get({ page: page, rank: rank }, function(data) {
+			Api.save({ page: page, rank: JSON.stringify(rank) }, function(data) {
 				var newData = {
 					current_page: data.current_page,
 					my_position: data.my_position,
@@ -42,7 +47,6 @@ function RankingCtrl($scope, $window, $resource, $sce) {
 					ranking: data.ranking,
 					total: data.total
 				};
-				console.log("PAGINA: "+page);
 			    var newrank = eval("$scope.rankings." + data.ranking.name + " = " + JSON.stringify(newData));
 			});
 		}
