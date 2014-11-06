b2a96caedc503f928aedfaa65edb40dc363c71cf
Matteo Pozzana <mpozzana@shado.tv>
2014-11-06 17:55:50 +0100

    [CQ] added cache on superfan contest points

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -85,7 +85,7 @@ class ProfileController < ApplicationController
   end
   
   def superfan_contest
-    contest_points = cache_short(get_superfan_contest_point_key(current_user.id)) do
+    contest_points = cache_short(get_reward_points_for_user_key(SUPERFAN_CONTEST_REWARD, current_user.id)) do
       get_counter_about_user_reward(SUPERFAN_CONTEST_REWARD)
     end
     @points_to_achieve = contest_points.nil? ? SUPERFAN_CONTEST_POINTS_TO_WIN : SUPERFAN_CONTEST_POINTS_TO_WIN - contest_points
