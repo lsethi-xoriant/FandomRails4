f3a40439905a971e00612048ac75afdc9350af2d
Matteo Pozzana <mpozzana@shado.tv>
2014-11-10 17:34:15 +0100

    [refs #465] Refactoring gallery functionality and fit it to work for ballando upload page

--- app/controllers/gallery_controller.rb
+++ app/controllers/gallery_controller.rb
@@ -10,7 +10,20 @@ class GalleryController < ApplicationController
   
   def show
     @calltoaction = CallToAction.find(params[:id])
+    @upload_interaction = @calltoaction.interactions.find_by_resource_type("Upload")
     @gallery_tag = get_tag_with_tag_about_call_to_action(@calltoaction, "gallery").first
+    @extra_info = cache_short(get_gallery_extra_info_key()) do
+      extra = Hash.new
+      TagField.where("tag_id = ?", @gallery_tag.id).each do |tf|
+        if tf.field_type == "STRINGA"
+          extra[tf.name] = tf.value
+        else
+          extra[tf.name] = tf.upload
+        end
+      end
+      extra
+    end
+    
     @gallery_calltoactions = calltoaction_active_with_tag(@gallery_tag.name, "DESC").where("user_id IS NOT NULL AND approved = true")
   end
   
