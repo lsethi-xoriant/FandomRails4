d265e61cb3211363449aaafba2dc5a3e935e7304
Alessandro Tolomio <atolomio@shado.tv>
2014-11-18 15:36:35 +0100

    [closes #504] Added forcing in landing

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -4,11 +4,8 @@
 require 'fandom_utils'
 
 class LandingController < ApplicationController
-  skip_before_filter :verify_authenticity_token
 
-  def landing_app
+  def index
   end
 
-  def landing_tab
-  end
 end
