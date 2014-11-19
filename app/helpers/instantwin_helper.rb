8f96a0b4fb263c261b90def80299a636ed57f6c3
Matteo Pozzana <mpozzana@shado.tv>
2014-11-19 15:56:25 +0100

    [refs #62] Instantwin helper minor fix in function documentation

--- app/helpers/instantwin_helper.rb
+++ app/helpers/instantwin_helper.rb
@@ -1,46 +1,51 @@
 module InstantwinHelper
 	
-	#
-	# Update the points of an user for the contests
-	#
-	# user_id - id of user
-	# points - points gained by the user
-	#
-	def update_contest_points user_id, points
-		begin	  
-		  Contest.active.each do |ac|
-		    contestpoints = ContestPoint.where("user_id=? AND contest_id=?", user_id, ac.id)
-	      if contestpoints.count > 0
-	        contestpoints.first.update_attribute(:points, contestpoints.first.points + points)
-	      else
-	        ContestPoint.create(:user_id => user_id, :contest_id => ac.id, :points => points)
-	      end  
-		  end
-		rescue Exception => e
-		end	  
-	end
+	DAYS_IN_MONTH = [nil, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+	
+	# Returns days in a month
+  #
+  # month - month want to know days amount
+  # year - specific year 
+  def days_in_month(month, year = Time.now.year)
+   return 29 if month == 2 && Date.gregorian_leap?(year)
+   DAYS_IN_MONTH[month]
+  end
 	
 	#
 	# Get number of ticket for a specific user on a specific contest
 	#
 	# user_id - id of current user
+	# interaction_id - id of interaction instantwin 
 	#
-	def get_current_contest_points user_id
-	  active_contests = cache_short { Contest.active.to_a }
-		if active_contests.any? && (cp = ContestPoint.find_by_user_id_and_contest_id(user_id, active_contests.first.id))
-		   return cp.points/active_contests.first.conversion_rate
-		else
-			return 0
-		end
+	def has_tickets(interaction_id)
+	  reward_name = get_reward_name_for_contest(interaction_id)
+	  tickets = get_counter_about_user_reward(reward_name)
+	  tickets > 0
 	end
 	
-	# TODO MAXIBON
+	def get_reward_name_for_contest(interaction_id)
+	  cache_short(get_reward_name_for_contest_key(interaction_id)) do
+	    Interaction.find(interaction_id).resource.reward.name
+	  end
+	end
+	
 	#
 	# Check if a user has already won a prize in the active contes
 	#
 	# user_id - id of current user
-	def check_user_already_won user_id
-	  return PlayticketEvent.where("user_id = ? AND winner = true", user_id).present?
+	# interaction_id - id of interaction instantwin
+	#
+	def user_already_won(interaction_id)
+	  winner = cache_short(get_user_already_won_contest(current_user.id, interaction_id)) do
+	    UserInteraction.where("interaction_id = ? AND user_id = ? AND (aux->>'instant_win_id') IS NOT NULL", interaction_id, current_user.id).to_a
+	  end
+	  winner.count > 0
 	end
+
+  def deduct_ticket(reward_name)
+    get_reward_with_periods(reward_name).each do |reward|
+      reward.update_attribute(:counter, reward.counter - 1)
+    end
+  end
 
 end
