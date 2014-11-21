2637a3b229258aa5d6db1f37165996e1fef59ea7
Alessandro Tolomio <atolomio@shado.tv>
2014-11-21 10:00:00 +0100

    [refs #519] Added privacy policy page

--- app/controllers/sites/coin/application_controller.rb
+++ app/controllers/sites/coin/application_controller.rb
@@ -18,5 +18,8 @@ class Sites::Coin::ApplicationController < ApplicationController
   def registration_fully_completed?
     current_user.province.present? && current_user.birth_date.present?
   end
+
+  def show_privacy_policy
+  end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Fandom::Application.routes.draw do
     scope module: "sites" do
       scope module: "coin" do
         root :to => "application#index"
+        match '/privacy_policy', :to => 'application#show_privacy_policy'
         devise_scope :user do
           post "/users", :to => "registrations#create"
           match 'auth/:provider/callback', :to => 'sessions#create'
