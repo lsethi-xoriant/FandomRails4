0a63e14a9be50d83be410cf2e90bce3a43034c40
Alessandro Tolomio <atolomio@shado.tv>
2014-12-02 09:27:34 +0100

    [refs #532] Added concorsi.coin.it

--- config/sites/coin.rb
+++ config/sites/coin.rb
@@ -3,7 +3,7 @@ module Fandom
     register_fandom_site(
       id: 'coin', 
       title: 'Coin Gift Machine',
-      domains: ['coin.fandom.localdomain', 'coin.dev.fandomlab.com', 'coin.stage.fandomlab.com', 'coin.live.fandomlab.com'],
+      domains: ['coin.fandom.localdomain', 'coin.dev.fandomlab.com', 'coin.stage.fandomlab.com', 'coin.live.fandomlab.com', 'concorsi.coin.it'],
       assets_precompile: ['coin_application.css', 'coin_application.js'],
       periodicity_kinds: [PERIOD_KIND_DAILY],
       required_attrs: ["first_name", "last_name", "province"],
