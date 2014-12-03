85593df1042a6426f51c4ccd7f93ae444ddade18
Alessandro Tolomio <atolomio@shado.tv>
2014-12-03 16:28:48 +0100

    [refs #538] Added cookie page

--- config/sites/coin.rb
+++ config/sites/coin.rb
@@ -8,7 +8,7 @@ module Fandom
       periodicity_kinds: [PERIOD_KIND_DAILY],
       required_attrs: ["first_name", "last_name", "province"],
       init_ctas: 1,
-      public_pages: Set.new(["sites/coin/application#show_privacy_policy", "sites/coin/application#show_stores"]),
+      public_pages: Set.new(["sites/coin/application#show_privacy_policy", "sites/coin/application#show_cookies_policy", "sites/coin/application#show_stores"]),
       force_landing: true,
       environment: { 
         'EMAIL_ADDRESS' => 'noreply@coin.it'
