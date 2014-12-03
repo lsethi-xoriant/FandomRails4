63d51d978dd24d31831e177e174dbac30ddb50f0
Alessandro Tolomio <atolomio@shado.tv>
2014-12-03 11:43:49 +0100

    [closes #563] Changed win email subject

--- app/mailers/system_mailer.rb
+++ app/mailers/system_mailer.rb
@@ -21,7 +21,7 @@ class SystemMailer < ActionMailer::Base
   	@cuser = user
   	@ticket_id = time_to_win
   	
-  	mail(to: user.email, bcc: 'contestfandom@gmail.com', subject: "#{subject} - #{@reward.title}")
+  	mail(to: user.email, bcc: 'contestfandom@gmail.com', subject: "#{subject} - Hai vinto #{@reward.title}")
   end
 
   def win_admin_notice_mail(user, reward, time_to_win, request)
