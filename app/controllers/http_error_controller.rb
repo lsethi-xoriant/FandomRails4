9fdebeb60f5c14078179e3af94d811f63dd44fcf
Diego Dainese <ddainese@shado.tv>
2014-12-10 12:51:03 +0100

    [closes #600] page error handling for json services

--- app/controllers/http_error_controller.rb
+++ app/controllers/http_error_controller.rb
@@ -1,12 +1,22 @@
 class HttpErrorController < ApplicationController
 
   def not_found_404
+    return_error(404, 'page not found')
   end
   
   def internal_error_500
+    return_error(500, 'something bad happened')
   end
   
   def unprocessable_entity_422
+    return_error(422, 'unprocessable entity')
+  end
+
+  def return_error(code, message)
+    respond_to do |format|
+      format.html
+      format.json { render json: { error_code: code, error_message: message } }
+    end
   end
 
 end
