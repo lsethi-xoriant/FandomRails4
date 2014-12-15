d7b2fd7a8b681cf7e892779c7f316a1d7bb66055
Matteo Pozzana <mpozzana@shado.tv>
2014-12-15 15:19:21 +0100

    [refs #105] Changed paperclip preset to fit ratio and graphic image dimensions

--- app/models/tag_field.rb
+++ app/models/tag_field.rb
@@ -1,7 +1,19 @@
 class TagField < ActiveRecord::Base
   attr_accessible :name, :field_type, :value, :tag_id, :upload
   
-  has_attached_file :upload #, :styles => { :medium => "400x400>", :thumb => "100x100>" }, :default_url => ""
+  has_attached_file :upload,
+    :styles => { 
+      :original => "100%", 
+      :carousel => "1024x320^", 
+      :medium => "524x393^", 
+      :thumb => "262x147^" 
+    }, 
+    :convert_options => { 
+      :original => " -quality 60", 
+      :carousel => " -gravity center -crop '1024x320+0+0' -quality 60", 
+      :medium => " -gravity center -crop '524x393+0+0' -quality 60", 
+      :thumb => " -gravity center -crop '262x147+0+0' -quality 60" 
+    }
 
   validates_presence_of :name
   
