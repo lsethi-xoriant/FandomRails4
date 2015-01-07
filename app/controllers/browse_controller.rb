908b68ec4c8f0e2dc69a304740f65a406ad314fb
Alessandro Tolomio <atolomio@shado.tv>
2015-01-07 16:55:20 +0100

    [FD-640] Moved browse function from controller to helper

--- app/controllers/browse_controller.rb
+++ app/controllers/browse_controller.rb
@@ -34,55 +34,6 @@ class BrowseController < ApplicationController
     @contents = get_contents_by_category(@tag)
   end
   
-  def merge_contents(ctas,tags)
-    merged = (ctas + tags).sort_by(&:created_at)
-    prepare_contents(merged)
-  end
-  
-  def merge_contents_with_tags(ctas,tags)
-    merged = (ctas + tags).sort_by(&:created_at)
-    prepare_contents_with_related_tags(merged)
-  end
-  
-  def merge_search_contents(ctas, tags, offset, limit)
-    merged = (ctas + tags).sort_by(&:created_at)
-    prepare_contents(merged)
-  end
-  
-  def prepare_contents_with_related_tags(elements)
-    contents = []
-    tags = {}
-    elements.each do |element|
-      if element.class.name == "CallToAction"
-        contents << cta_to_category(element)
-        tags = addCtaTags(tags, element)
-      else
-        contents << tag_to_category(element)
-        tags = addTagTags(tags, element)
-      end
-    end
-    [contents, tags]
-  end
-  
-  def addCtaTags(tags, element)
-    element.call_to_action_tags.includes(:tag).each do |t|
-      if !tags.has_key?(t.tag.id)
-        tags[t.tag.id] = get_extra_fields!(t.tag).fetch("title", t.tag.name)
-      end
-    end
-    tags
-  end
-  
-  def addTagTags(tags, element)
-    element.tags_tags.each do |t|
-      other_tag = Tag.find(t.other_tag_id)
-      if !tags.has_key?(other_tag.id)
-        tags[t.other_tag_id] = get_extra_fields!(other_tag).fetch("title", other_tag.name)
-      end
-    end
-    tags
-  end
-  
   def search
     term = params[:q]
     results = cache_short(get_browse_search_results_key(term)) { get_contents_by_query(term) }
