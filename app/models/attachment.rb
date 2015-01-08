eedfb142f162d392fc6c5722e1013ce95211424e
Matteo Pozzana <mpozzana@shado.tv>
2015-01-08 11:54:49 +0100

    [FD-623] Fixed paperclip crop and css alignment for images in order to fit the context container

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -6,8 +6,19 @@ class Attachment < ActiveRecord::Base
 
   attr_accessor :destroy_data
 
-  has_attached_file :data, :styles => { :large => "600x600#", :medium => "300x300#", :custom => "262x147#", :thumb => "100x100#" }, 
-                    :convert_options => { :large => '-quality 60', :medium => '-quality 60', :thumb => '-quality 60' }
+  has_attached_file :data, 
+    :styles => { 
+      :original => "100%", 
+      :carousel => "1024x320^", 
+      :medium => "524x393^", 
+      :thumb => "262x147^" 
+    }, 
+    :convert_options => { 
+      :original => " -quality 60", 
+      :carousel => " -crop '1024x320+0+40' -quality 60", 
+      :medium => " -gravity center -crop '524x393+0+0' -quality 60", 
+      :thumb => " -gravity center -crop '262x147+0+0' -quality 60" 
+    }
 
   def destroy_data=(destroy_data_check)
     self.data.destroy if destroy_data_check == '1'
