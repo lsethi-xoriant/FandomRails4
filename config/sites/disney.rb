16f231994fe387717447513d4dc1d5bb84e04fd0
Alessandro Tolomio <atolomio@shado.tv>
2015-01-09 14:32:50 +0100

    [CQ] Fixed syntax error

--- config/sites/disney.rb
+++ config/sites/disney.rb
@@ -2,7 +2,7 @@ module Fandom
   class Application < Rails::Application
     register_fandom_site(
       id: 'disney', 
-      domains: ['disney.fandom.localdomain', 'disney.stage.fandomlab.com', 'disney.live.fandomlab.com', 'disney.dev.fandomlab.com', 'disney.fandom.disneychannel.it', 'disney.dev.dc.fandomlab.com', 'disney.stage.dc.fandomlab.com', 'disney.live.dc.fandomlab.com']
+      domains: ['disney.fandom.localdomain', 'disney.stage.fandomlab.com', 'disney.live.fandomlab.com', 'disney.dev.fandomlab.com', 'disney.fandom.disneychannel.it', 'disney.dev.dc.fandomlab.com', 'disney.stage.dc.fandomlab.com', 'disney.live.dc.fandomlab.com'],
       assets_precompile: ['disney_application.css', 'disney_application.js'],
       twitter_integration: true,
       allowed_context_roots: ["violetta"]
