56de3d2ee993491b61de9f8d03c8f6d14351942f
Alessandro Tolomio <atolomio@shado.tv>
2015-01-12 12:18:49 +0100

    [FD-648] Updated versus feedback

--- app/assets/javascripts/angularjs/stream_calltoaction.js
+++ app/assets/javascripts/angularjs/stream_calltoaction.js
@@ -1202,35 +1202,34 @@ function StreamCalltoactionCtrl($scope, $window, $http, $timeout, $interval) {
               updateAnonymousUserStorageUserInteractions(user_interaction_for_storage);
             } 
 
-            // Interaction after user response.
-            updateUserInteraction(calltoaction_id, interaction_id, data.user_interaction);
-            calltoaction_info.status = JSON.parse(data.calltoaction_status);
-            $scope.current_user.main_reward_counter = data.main_reward_counter;   
-
-            if(data.answers) {
-              updateAnswersInInteractionInfo(interaction_info, data.answers);
-            }
-            
             if(when_show_interaction == "OVERVIDEO_DURING" || when_show_interaction == "OVERVIDEO_END") {
-              if(interaction_info.interaction.resource_type == "trivia") {
-                angular.forEach(interaction_info.interaction.resource.answers, function(answer) {
-                  answer.class = "trivia-interaction__answer--visible";
-                });
-              } else if(interaction_info.interaction.resource_type == "versus") {
-                index = 0;
-                angular.forEach(interaction_info.interaction.resource.answers, function(answer) {
-                  if(index % 2 == 0) {
-                    answer.class = "versus-interaction__answer--visible-left";
-                  } else {
-                    answer.class = "versus-interaction__answer--visible-right";
-                  }
-                  index += 1;
-                });
-              }
+              
+              interaction_info.feedback = true;
 
-            	$timeout(function() { 
-    			      removeOvervideoInteraction(getPlayer(calltoaction_id), calltoaction_id, interaction_info);
-    			    }, 3000);
+              $timeout(function() { 
+                interaction_info.feedback = false;
+
+                if(interaction_info.interaction.resource_type == "trivia") {
+                  angular.forEach(interaction_info.interaction.resource.answers, function(answer) {
+                    answer.class = "trivia-interaction__answer--visible";
+                  });
+                } else if(interaction_info.interaction.resource_type == "versus") {
+                  index = 0;
+                  angular.forEach(interaction_info.interaction.resource.answers, function(answer) {
+                    if(index % 2 == 0) {
+                      answer.class = "versus-interaction__answer--visible-left";
+                    } else {
+                      answer.class = "versus-interaction__answer--visible-right";
+                    }
+                    index += 1;
+                  });
+                }
+
+                $timeout(function() { 
+                  removeOvervideoInteraction(getPlayer(calltoaction_id), calltoaction_id, interaction_info);
+                }, 3000);
+              }, 300000000000);
+
             } else {
               if(interaction_info.interaction.resource_type == "like") {
                 if(JSON.parse(interaction_info.user_interaction.aux)["like"]) {
@@ -1240,6 +1239,16 @@ function StreamCalltoactionCtrl($scope, $window, $http, $timeout, $interval) {
                 } 
               }
             }
+
+            // Interaction after user response.
+            updateUserInteraction(calltoaction_id, interaction_id, data.user_interaction);
+            calltoaction_info.status = JSON.parse(data.calltoaction_status);
+            $scope.current_user.main_reward_counter = data.main_reward_counter;   
+
+            if(data.answers) {
+              updateAnswersInInteractionInfo(interaction_info, data.answers);
+            }
+            
 
             /*
             if(data.download_interaction_attachment) {
@@ -1295,7 +1304,7 @@ function StreamCalltoactionCtrl($scope, $window, $http, $timeout, $interval) {
   };
   
   $window.updateVote = function(call_to_action_id, interaction_id, when_show_interaction){
-  	var vote = $("#interaction-"+interaction_id+"-vote-value").val();
+  	var vote = $("#interaction-" + interaction_id + "-vote-value").val();
   	updateAnswer(call_to_action_id, interaction_id, vote, when_show_interaction);
   };
 
