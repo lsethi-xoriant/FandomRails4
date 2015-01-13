9a7970dc6dcc0d2488e8ca2e17890f8a66cdfec6
Matteo Pozzana <mpozzana@shado.tv>
2015-01-13 17:54:31 +0100

    [FD-608] Added notices route for disney

--- config/routes.rb
+++ config/routes.rb
@@ -31,6 +31,7 @@ Fandom::Application.routes.draw do
         match "/iur", to: "application#iur"
         match "profile/rankings", :to => "profile#rankings"
         match "profile/rewards", :to => "profile#rewards"
+        match "profile/notices", :to => "profile#notices"
         devise_scope :user do
           match "/iur/sign_in", to: "registrations#iur"
         end
