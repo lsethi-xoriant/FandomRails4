1b47b3c9ff8204fac9e1a9fcb3fc91acd0808018
Matteo Pozzana <mpozzana@shado.tv>
2015-01-13 17:44:09 +0100

    [FD-608] Fixed update user name profile, and level bars and ordering in profile rewards

--- app/controllers/sites/disney/profile_controller.rb
+++ app/controllers/sites/disney/profile_controller.rb
@@ -20,11 +20,40 @@ class Sites::Disney::ProfileController < ProfileController
   
   def rewards
     levels, levels_use_prop = rewards_by_tag("level")
-    @levels = levels.nil? ? nil : levels[get_current_property]
+    @levels = prepare_levels_to_show(levels)
     @my_levels = get_other_property_rewards("level")
     badges, badges_use_prop = rewards_by_tag("badge")
     @badges = badges.nil? ? nil : badges[get_current_property]
     @mybadges = get_other_property_rewards("badge")
+  end
+  
+  def prepare_levels_to_show(levels)
+    levels = levels[get_current_property] 
+    order_rewards(levels.to_a, "cost")
+    prepared_levels = {}
+    if levels
+      index = 0
+      level_before_point = 0
+      level_before_status = nil
+      levels.each do |level|
+        if level_before_status.nil? || level_before_status == "gained"
+          progress = disney_calculate_level_progress(level, level_before_point, get_current_property)
+          if progress > 100
+            level_before_status = "gained"
+            prepared_levels["#{index+1}"] = {"level" => level, "level_number" => index+1, "progress" => 100, "status" => level_before_status }
+          else
+            level_before_status = "progress"
+            prepared_levels["#{index+1}"] = {"level" => level, "level_number" => index+1, "progress" => progress, "status" => level_before_status }
+          end
+        else
+          progress = 0
+          level_before_status = "locked"
+          prepared_levels["#{index+1}"] = {"level" => level, "level_number" => index+1, "progress" => progress, "status" => level_before_status }
+        end
+        index += 1
+      end
+    end
+    prepared_levels
   end
   
   def get_other_property_rewards(reward_name)
