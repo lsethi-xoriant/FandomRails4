c315ffae99a4a130c9c4f71adcbc3295c57f1d62
Diego Dainese <ddainese@shado.tv>
2015-01-15 14:58:02 +0100

    [FD-706] fixed a syntax error

--- lib/fandom_utils.rb
+++ lib/fandom_utils.rb
@@ -164,7 +164,7 @@ module FandomUtils
   end    
   
   def reset_migrations_table_for_tenant(tenant_id = nil)
-    schema = tenant_id.nil? '' : tenant_id + '.'
+    schema = tenant_id.nil? ? '' : tenant_id + '.'
     Dir.open('db/migrate').each do |fname|
        i = fname.split('_').first.to_i
        next if i == 0
