919a56179e541cecd308f7bb2612bbe54fbd4782
Matteo Pozzana <mpozzana@shado.tv>
2015-01-19 10:31:03 +0100

    [FD-145] Now in dev works catalogue page. Expire cache credits fix.

--- app/controllers/reward_controller.rb
+++ app/controllers/reward_controller.rb
@@ -95,11 +95,11 @@ class RewardController < ApplicationController
         period_reward.update_attribute(:counter, period_reward.counter - reward.cost)
       end
       UserReward.create(user_id: current_user.id, reward_id: reward.id, available: true, counter: 1)
+      expire_cache_key(get_reward_points_for_user_key(reward.currency.name, current_user.id))
       response["html"] = "<p class=\"cta-preview__unlocked-message\">PREMIO SBLOCCATO</p>
       <p><small>Hai speso #{reward.cost} #{reward.currency.name}</small></p>
       <p><small>Hai ancora #{get_counter_about_user_reward(reward.currency.name)} #{reward.currency.name}</small></p>
       <button class=\"btn btn-primary\" onclick=\"javascript:location.reload();\">Scopri il premio</button>".html_safe
-      expire_cache_key(get_reward_points_for_user_key(reward.currency.name, current_user.id))
     else
       response["html"] = "<p>Non hai abbastanza #{reward.currency.name} per sbloccare questo premio</p>
       <div class=\"label cta-preview__credits--reward\">+ #{reward.cost}<i class=\"fa fa-copyright\"></i></div>".html_safe
