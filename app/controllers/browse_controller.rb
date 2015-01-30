33df66afbdea9e714b82556181cca9c618df4339
Matteo Pozzana <mpozzana@shado.tv>
2015-01-30 17:03:15 +0100

    [FD-786] Fixes load more cta status in index category

--- app/controllers/browse_controller.rb
+++ app/controllers/browse_controller.rb
@@ -119,6 +119,28 @@ class BrowseController < ApplicationController
     offset = params[:offset].to_i
     category = Tag.find(params[:tag_id])
     contents, tags = get_contents_by_category_with_tags(category, offset)
+    
+    if INDEX_CATEGORY_CTA_STATUS_ACTIVE
+      cta_ids = []
+      contents.each do |content|
+        if content["type"] == "cta"
+          cta_ids << content["id"]
+        end
+      end
+      cta_statuses = {}
+      unless cta_ids.empty?
+        cta_statuses = cta_to_reward_statuses_by_user(current_or_anonymous_user, CallToAction.includes(:interactions).where("id in (?)", cta_ids).to_a, 'point')
+      end
+      contents.each do |content|
+        if content["type"] == "cta"
+          content["status"] = cta_statuses[content["id"].to_i]
+        end
+      end
+      contents = contents
+    else
+      contens = contents
+    end
+    
     respond_to do |format|
       format.json { render :json => contents.to_json }
     end
