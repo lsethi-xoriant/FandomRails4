04be9ec9b2f445d99d13d012f390bb3cd6abf572
Diego Dainese <ddainese@shado.tv>
2015-02-02 14:15:36 +0100

    [FD-797] log_"content"_viewed() now checks if the id is an int
    the check is needed to avoid log_daemon to fail handling content view
    counters

--- app/helpers/log_helper.rb
+++ app/helpers/log_helper.rb
@@ -1,7 +1,12 @@
 module LogHelper
   
   def log_call_to_action_viewed(id)
-    log_audit(LOG_MESSAGE_CONTENT_VIEWED, { type: 'cta', id: id })
+    begin
+      id_as_int = Integer(id) # Integer() is used instead of to_i() to raise an exception if id is not a number
+      log_audit(LOG_MESSAGE_CONTENT_VIEWED, { type: 'cta', id: id_as_int }) 
+    rescue
+      log_error('attempt to log a cta viewed with a wrong id', { id: id })
+    end      
   end
 
 end
