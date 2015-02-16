292118d4d1b971669a89e21623e35765544c301c
Alessandro Tolomio <atolomio@shado.tv>
2015-02-16 16:42:52 +0100

    [FD-835] Moved typography folder into vendor

--- config/sites/disney.rb
+++ config/sites/disney.rb
@@ -3,7 +3,7 @@ module Fandom
     register_fandom_site(
       id: 'disney', 
       domains: ['disney.fandom.localdomain', 'disney.stage.fandomlab.com', 'disney.live.fandomlab.com', 'disney.dev.fandomlab.com', 'disney.fandom.disneychannel.it', 'disney.dev.dc.fandomlab.com', 'disney.stage.dc.fandomlab.com', 'disney.live.dc.fandomlab.com', 'community.disneychannel.it', 'disney.fandom.disneychannel.it'],
-      assets_precompile: ['disney_application.css', 'disney_application.js', 'ie9.css'],
+      assets_precompile: ['disney_application.css', 'disney_application.js', 'disney_ie9.css'],
       twitter_integration: true,
       allowed_context_roots: ["violetta"],
       init_ctas: 6,
