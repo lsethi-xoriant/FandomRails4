6fd61ca141f23a25e1e8973758f204dceedefda0
Diego Dainese <ddainese@shado.tv>
2015-02-16 17:35:04 +0100

    [FD-876] added domains for orzoro

--- config/sites/orzoro.rb
+++ config/sites/orzoro.rb
@@ -2,7 +2,7 @@ module Fandom
   class Application < Rails::Application
     register_fandom_site(
       id: 'orzoro', 
-      domains: ['orzoro.fandom.localdomain', 'orzoro.stage.fandomlab.com', 'orzoro.live.fandomlab.com', 'orzoro.dev.fandomlab.com'],
+      domains: ['orzoro.fandom.localdomain', 'orzoro.stage.fandomlab.com', 'orzoro.live.fandomlab.com', 'orzoro.dev.fandomlab.com', 'preprod.orzoro.it'],
       assets_precompile: ['application.css', 'application.js', 'ie9.css'],
       anonymous_interaction: true,
       free_provider_share: true
