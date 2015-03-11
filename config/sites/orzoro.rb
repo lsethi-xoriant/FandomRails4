4e9759081b7ec793131fe8bd37321df046baa920
Diego Dainese <ddainese@shado.tv>
2015-03-11 11:32:53 +0100

    [FD-973] added www.orzoro.it to supported orzoro domains

--- config/sites/orzoro.rb
+++ config/sites/orzoro.rb
@@ -2,7 +2,7 @@ module Fandom
   class Application < Rails::Application
     register_fandom_site(
       id: 'orzoro', 
-      domains: ['orzoro.fandom.localdomain', 'orzoro.stage.fandomlab.com', 'orzoro.live.fandomlab.com', 'orzoro.dev.fandomlab.com', 'preprod.orzoro.it'],
+      domains: ['orzoro.fandom.localdomain', 'orzoro.stage.fandomlab.com', 'orzoro.live.fandomlab.com', 'orzoro.dev.fandomlab.com', 'preprod.orzoro.it', 'www.orzoro.it'],
       assets_precompile: ['orzoro_application.css', 'orzoro_application.js', 'orzoro_ie9.css'],
       anonymous_interaction: true,
       periodicity_kinds: [PERIOD_KIND_DAILY],
