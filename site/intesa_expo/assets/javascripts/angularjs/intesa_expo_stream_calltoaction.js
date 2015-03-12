9c2501b659239239f23382c2fc978ed209582691
Alessandro Tolomio <atolomio@shado.tv>
2015-03-12 18:22:09 +0100

    [FD-982] Init Intesa Expo project

---
+++ site/intesa_expo/assets/javascripts/angularjs/intesa_expo_stream_calltoaction.js
@@ -0,0 +1,18 @@
+var intesaExpoStreamCalltoactionModule = angular.module('IntesaExpoStreamCalltoactionModule', ['ngRoute', 'ngSanitize', 'ngAnimate']);
+
+IntesaExpoStreamCalltoactionCtrl.$inject = ['$scope', '$window', '$http', '$timeout', '$interval', '$sce'];
+intesaExpoStreamCalltoactionModule.controller('IntesaExpoStreamCalltoactionCtrl', IntesaExpoStreamCalltoactionCtrl);
+
+// csrf-token handling in ajax calls
+intesaExpoStreamCalltoactionModule.config(["$httpProvider", function(provider) {
+  provider.defaults.headers.common['X-CSRF-Token'] = $('meta[name=csrf-token]').attr('content');
+}]);
+
+
+function IntesaExpoStreamCalltoactionCtrl($scope, $window, $http, $timeout, $interval, $document) {
+  angular.extend(this, new StreamCalltoactionCtrl($scope, $window, $http, $timeout, $interval, $document));
+
+  $scope.extraInit = function() {
+  };
+
+}
