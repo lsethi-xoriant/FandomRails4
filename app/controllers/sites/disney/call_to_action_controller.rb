1f2f979be3a0f0e58c14ddfed6eb557d71e3fad0
Alessandro Tolomio <atolomio@shado.tv>
2015-03-16 10:31:39 +0100

    [FD-992] Upgrade append other with filter

--- app/controllers/sites/disney/call_to_action_controller.rb
+++ app/controllers/sites/disney/call_to_action_controller.rb
@@ -2,56 +2,10 @@
 class Sites::Disney::CallToActionController < CallToActionController
   include DisneyHelper
 
-  def ordering_ctas
-    if params["other_params"] && (other_params = JSON.parse(params["other_params"])["gallery"]["calltoaction_id"])
-      gallery_calltoaction_id = other_params
-    end
-
-    property = get_tag_from_params(get_disney_property())
-    init_ctas = $site.init_ctas
-
-    calltoactions = []
-    ordering = params["ordering"]
-
-    case ordering
-    when "comment"
-      calltoactions = cache_short(get_calltoactions_in_property_by_ordering_cache_key(property.id, ordering, gallery_calltoaction_id)) do
-        calltoaction_ids = get_disney_ctas(property, gallery_calltoaction_id).map { |calltoaction| calltoaction.id }.join(",")
-        sql = "SELECT call_to_actions.id,  sum((user_comment_interactions.approved is not null and user_comment_interactions.approved)::integer) " +
-              "FROM call_to_actions LEFT OUTER JOIN interactions ON call_to_actions.id = interactions.call_to_action_id LEFT OUTER JOIN user_comment_interactions ON interactions.resource_id = user_comment_interactions.comment_id " +
-              "WHERE interactions.resource_type = 'Comment' AND call_to_actions.id in (#{calltoaction_ids}) " +
-              "GROUP BY call_to_actions.id " +
-              "ORDER BY sum DESC limit #{init_ctas};"
-        execute_sql_and_get_ctas_ordered(sql)
-      end
-    when "view"
-      calltoactions = cache_short(get_calltoactions_in_property_by_ordering_cache_key(property.id, ordering, gallery_calltoaction_id)) do
-        calltoaction_ids = get_disney_ctas(property, gallery_calltoaction_id).map { |calltoaction| calltoaction.id }.join(",")
-        sql = "SELECT call_to_actions.id " +
-              "FROM call_to_actions LEFT OUTER JOIN view_counters ON call_to_actions.id = view_counters.ref_id " +
-              "WHERE (view_counters.ref_type is null OR view_counters.ref_type = 'cta') AND call_to_actions.id in (#{calltoaction_ids}) " +
-              "ORDER BY (coalesce(view_counters.counter, 0) / (extract('epoch' from (now() - coalesce(call_to_actions.activated_at, call_to_actions.created_at) )) / 3600 / 24)), call_to_actions.activated_at DESC limit #{init_ctas};"
-        execute_sql_and_get_ctas_ordered(sql)
-      end
-    else
-      calltoactions = cache_medium(get_calltoactions_in_property_cache_key(property.id, gallery_calltoaction_id, get_cta_max_updated_at())) do
-        get_disney_ctas(property, gallery_calltoaction_id).limit(init_ctas).to_a
-      end
-    end
-
-    response = {
-      calltoaction_info_list: build_call_to_action_info_list(calltoactions, ["like", "comment", "share"])
-    }
-    
-    respond_to do |format|
-      format.json { render json: response.to_json }
-    end 
-  end
-
   def execute_sql_and_get_ctas_ordered(sql)
     cta_ids = ActiveRecord::Base.connection.execute(sql)
     order =  cta_ids.map { |r| "call_to_actions.id = #{r["id"].to_i} DESC" }
-    CallToAction.where("call_to_actions.id IN (?)", cta_ids.map { |r| r["id"].to_i }).order("#{order.join(",")}").to_a
+    CallToAction.where("call_to_actions.id" => cta_ids.map { |r| r["id"].to_i }).order("#{order.join(",")}").to_a
   end
 
   def build_current_user() 
@@ -104,37 +58,64 @@ class Sites::Disney::CallToActionController < CallToActionController
 
     ordering = params[:ordering]
 
-    response = cache_short(get_next_ctas_stream_for_user_cache_key(current_or_anonymous_user.id, property.id, calltoaction_ids_shown.last, get_cta_max_updated_at(), ordering, gallery_calltoaction_id)) do
-      calltoactions = cache_short(get_next_ctas_stream_cache_key(property.id, calltoaction_ids_shown.last, get_cta_max_updated_at(), ordering, gallery_calltoaction_id)) do     
-        calltoactions = get_disney_ctas(property, gallery_calltoaction_id).where("call_to_actions.id NOT IN (#{calltoaction_ids_shown_qmarks})", *calltoaction_ids_shown)
-        case ordering
-        when "comment"
-          calltoaction_ids = calltoactions.map { |calltoaction| calltoaction.id }.join(",")
-          sql = "SELECT call_to_actions.id, sum((user_comment_interactions.approved is not null and user_comment_interactions.approved)::integer) " +
-                "FROM call_to_actions LEFT OUTER JOIN interactions ON call_to_actions.id = interactions.call_to_action_id LEFT OUTER JOIN user_comment_interactions ON interactions.resource_id = user_comment_interactions.comment_id " +
-                "WHERE interactions.resource_type = 'Comment' AND call_to_actions.id in (#{calltoaction_ids}) " +
-                "GROUP BY call_to_actions.id " +
-                "ORDER BY sum DESC limit 4;"
-          execute_sql_and_get_ctas_ordered(sql)
-        when "view"
-          calltoaction_ids = calltoactions.map { |calltoaction| calltoaction.id }.join(",")
-          sql = "SELECT call_to_actions.id " +
-              "FROM call_to_actions LEFT OUTER JOIN view_counters ON call_to_actions.id = view_counters.ref_id " +
-              "WHERE (view_counters.ref_type is null OR view_counters.ref_type = 'cta') AND call_to_actions.id in (#{calltoaction_ids}) " +
-              "ORDER BY (coalesce(view_counters.counter, 0) / (extract('epoch' from (now() - coalesce(call_to_actions.activated_at, call_to_actions.created_at) )) / 3600 / 24)), call_to_actions.activated_at DESC limit 4;"
-        else
-          calltoactions = calltoactions.limit(4).to_a
-        end
-        calltoactions
+    calltoactions = cache_short(get_next_ctas_stream_cache_key(property.id, calltoaction_ids_shown.last, get_cta_max_updated_at(), ordering, gallery_calltoaction_id)) do     
+      calltoactions = get_disney_ctas(property, gallery_calltoaction_id).where("call_to_actions.id NOT IN (#{calltoaction_ids_shown_qmarks})", *calltoaction_ids_shown)
+      case ordering
+      when "comment"
+        calltoaction_ids = calltoactions.map { |calltoaction| calltoaction.id }.join(",")
+        gets_ctas_ordered_by_comments(calltoaction_ids, 4)
+      when "view"
+        calltoaction_ids = calltoactions.map { |calltoaction| calltoaction.id }.join(",")
+        gets_ctas_ordered_by_views(calltoaction_ids, 4)
+      else
+        calltoactions = calltoactions.limit(4).to_a
       end
-      
-      {
-        calltoaction_info_list: build_call_to_action_info_list(calltoactions, page_elements)
-      }.to_json
+      calltoactions
     end
+
+    response = {
+      calltoaction_info_list: build_call_to_action_info_list(calltoactions, page_elements)
+    }.to_json
     
     respond_to do |format|
       format.json { render json: response }
+    end 
+  end
+
+  def ordering_ctas
+    if params["other_params"] && (other_params = JSON.parse(params["other_params"])["gallery"]["calltoaction_id"])
+      gallery_calltoaction_id = other_params
+    end
+
+    property = get_tag_from_params(get_disney_property())
+    init_ctas = $site.init_ctas
+
+    calltoactions = []
+    ordering = params["ordering"]
+
+    case ordering
+    when "comment"
+      calltoactions = cache_short(get_calltoactions_in_property_by_ordering_cache_key(property.id, ordering, gallery_calltoaction_id)) do
+        calltoaction_ids = get_disney_ctas(property, gallery_calltoaction_id).map { |calltoaction| calltoaction.id }.join(",")
+        gets_ctas_ordered_by_comments(calltoaction_ids, init_ctas)
+      end
+    when "view"
+      calltoactions = cache_short(get_calltoactions_in_property_by_ordering_cache_key(property.id, ordering, gallery_calltoaction_id)) do
+        calltoaction_ids = get_disney_ctas(property, gallery_calltoaction_id).map { |calltoaction| calltoaction.id }.join(",")
+        gets_ctas_ordered_by_views(calltoaction_ids, init_ctas)
+      end
+    else
+      calltoactions = cache_medium(get_calltoactions_in_property_cache_key(property.id, gallery_calltoaction_id, get_cta_max_updated_at())) do
+        get_disney_ctas(property, gallery_calltoaction_id).limit(init_ctas).to_a
+      end
+    end
+
+    response = {
+      calltoaction_info_list: build_call_to_action_info_list(calltoactions, ["like", "comment", "share"])
+    }
+    
+    respond_to do |format|
+      format.json { render json: response.to_json }
     end 
   end
   
