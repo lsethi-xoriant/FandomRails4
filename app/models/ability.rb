00525575fa0d7829920d93edba8f8dac5257fac1
Filippo Ragazzo <filipporagazzo89@gmail.com>
2015-03-24 16:57:58 +0100

    [FD-1026] TomDoc documentation for CanCan abilities definition

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,12 +1,28 @@
+# Public: Class useful to define user permissions for CanCan gem usage.
+# CanCan documentation at https://github.com/ryanb/cancan.
 class Ability
-
-  # List of custom abilities:
-  # :access, :rails_admin, :easyadmin, :dashboard
-  # :manage, :all - :users - :published - :carousel - :call_to_actions - :user_call_to_actions
-  #           :promocodes - :rewards - :tags - :comments - :notices - :rankings - :settings
 
   include CanCan::Ability
 
+# Public: Initialize user permissions
+# List of custom abilities: 
+# :access, :rails_admin - :easyadmin - :dashboard
+# :manage, :all - :users - :published - :carousel - :call_to_actions - :user_call_to_actions
+#          :promocodes - :rewards - :tags - :comments - :notices - :rankings - :settings
+#
+# Examples
+# 
+#   before_filter :authorize_user
+#   def authorize_user
+#     authorize! :manage, :rewards
+#   end
+#   ...
+#
+#   def dashboard
+#     authorize! :access, :dashboard
+#     ...
+#
+# user - The User trying to access a page or using a method that requires permissions.
   def initialize(user)
 
     user ||= User.new # User not logged.
@@ -20,14 +36,14 @@ class Ability
       cannot :manage, :settings
       cannot :manage, :users
       can :read, :users
-    elsif user && user.role == "viewer"
-      can :access, :easyadmin
-      can :access, :dashboard
-      can :read, :users
     elsif user && user.role == "moderator"
       can :access, :easyadmin
       can :manage, :user_call_to_actions
       can :manage, :comments
+    elsif user && user.role == "viewer"
+      can :access, :easyadmin
+      can :access, :dashboard
+      can :read, :users
     end
  
   end
