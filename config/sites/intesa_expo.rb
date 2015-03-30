fcd30d0edebf4f0dab2d887632dbe904569594f1
Diego Dainese <ddainese@shado.tv>
2015-03-30 11:04:29 +0200

    [FD-982] created the stage domain for intesa-expo tenant

--- config/sites/intesa_expo.rb
+++ config/sites/intesa_expo.rb
@@ -2,7 +2,7 @@ module Fandom
   class Application < Rails::Application
     register_fandom_site(
       id: 'intesa_expo', 
-      domains: ['intesa-expo.fandom.localdomain', 'intesa-expo.live.fandomlab.com', 'intesa-expo.dev.fandomlab.com'],
+      domains: ['intesa-expo.fandom.localdomain', 'intesa-expo.live.fandomlab.com', 'intesa-expo.dev.fandomlab.com', 'intesa-expo.stage.fandomlab.com'],
       assets_precompile: ['intesa_expo_application.css', 'intesa_expo_application.js', 'intesa_expo_ie9.css'],
       periodicity_kinds: [PERIOD_KIND_DAILY],
       allowed_context_roots: ["it", "en", "imprese"],
