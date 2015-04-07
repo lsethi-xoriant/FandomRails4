87f8d9f97c27b58b60cd8e99c8d5afc2b2e47dfb
Filippo Ragazzo <filipporagazzo89@gmail.com>
2015-04-07 10:13:33 +0200

    [FD-908] removed interval selection from disney dashboard page

--- app/controllers/sites/disney/easyadmin/easyadmin_controller.rb
+++ app/controllers/sites/disney/easyadmin/easyadmin_controller.rb
@@ -22,20 +22,17 @@ class Sites::Disney::Easyadmin::EasyadminController < Easyadmin::EasyadminContro
 
       flash[:notice] = @values["migration_day"] == true ? 
                         "Hai selezionato un periodo che comprende o precede il giorno di migrazione,
-                          dunque i dati visualizzati comprendono tutte le statistiche precedenti a quel giorno"
+                          dunque i dati visualizzati comprendono anche tutte le statistiche precedenti a quel giorno"
                         : nil
 
       from = @from_date
       to = @to_date
 
-      if((to - from).to_i <= 7 && params[:time_interval] != "daily")
-        params[:time_interval] = "daily"
-      end
-
       hash_total_users = get_hash_total_users()
       hash_social_reg_users = get_hash_social_reg_users()
 
-      days_interval = (params[:time_interval] == "daily" && params[:commit] != "Reset") ? 1 : 7
+      @days = (to - from).to_i
+      days_interval = @days > 30 ? (@days / 30.0).round : 1
       @user_week_list = build_line_chart_values(from, to, hash_total_users, hash_social_reg_users, days_interval)
 
       @total_users = @values["total_users"]
