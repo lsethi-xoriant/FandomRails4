8dce8c108e92ee55d31493888a18a84177ead769
Alessandro Tolomio <atolomio@shado.tv>
2015-04-10 12:31:46 +0200

    [FD-1039] Add location to calendar and mobile single cta fix

--- app/helpers/calendar_helper.rb
+++ app/helpers/calendar_helper.rb
@@ -1,6 +1,6 @@
 module CalendarHelper
   
-  def build_ical(dt_start, dt_end, summary, description)
+  def build_ical(dt_start, dt_end, summary, description, location)
     cal = Icalendar::Calendar.new
     
     cal.version = "2.0"
@@ -11,6 +11,7 @@ module CalendarHelper
       e.dtend = dt_end #Icalendar::Values::Date.new(dt_end)
       e.summary = summary
       e.description = description
+      e.location = location
       e.ip_class = "PUBLIC"
 
       e.alarm do |a|
