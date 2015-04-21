99fec324520f2c3ec45e5faeda94a2658b0193c7
Filippo Ragazzo <filipporagazzo89@gmail.com>
2015-04-21 14:53:27 +0200

    [FD-1068] linked call to actions bug fix on edit cta page

--- app/controllers/easyadmin/call_to_action_controller.rb
+++ app/controllers/easyadmin/call_to_action_controller.rb
@@ -378,7 +378,8 @@ class Easyadmin::CallToActionController < Easyadmin::EasyadminController
     @cta.interactions.each do |interaction|
       InteractionCallToAction.where(:interaction_id => interaction.id).each do |icta|
         condition_hash = JSON.parse(icta.condition) rescue {}
-        @interaction_call_to_actions += [[condition_hash.keys.first, condition_hash.values.first, icta.call_to_action_id]]
+        @interaction_call_to_actions += 
+          [[(condition_hash.keys.first || ""), (condition_hash.values.first || ""), (icta.call_to_action_id || "")]]
       end
     end
     @tag_list_arr = Array.new
