4f8d411b7d2d6f03ce6ad87e00e64988a5831f20
Filippo Ragazzo <filipporagazzo89@gmail.com>
2015-05-13 12:36:51 +0200

    [FD-1230] update_cta_to_be_activated script

---
+++ bin/update_cta_to_be_activated.rb
@@ -0,0 +1,46 @@
+require 'yaml'
+require 'pg'
+
+def help_message
+  <<-EOF
+  Usage: #{$0} <configuration_file_path>
+  EOF
+end
+
+def main
+
+  if ARGV.count < 1
+    puts help_message
+    exit
+  end
+
+  config = YAML.load_file(ARGV[0].to_s)
+  conn = PG::Connection.open(config["db"])
+  now_in_utc = Time.now.utc
+
+  tenants = []
+  conn.exec("SELECT nspname FROM pg_namespace").each do |value|
+    unless (value["nspname"].start_with?("pg_") or value["nspname"] == "public" or value["nspname"] == "information_schema")
+      tenants << value["nspname"]
+    end
+  end
+
+  puts "Updating call to actions to be activated for the following tenants: #{tenants.join(",")}"
+
+  tenants.each do |tenant|
+    puts "Start #{tenant} call to actions updating"
+    start_time = Time.now
+    res = conn.exec(
+      "UPDATE #{tenant}.call_to_actions 
+      SET updated_at = '#{now_in_utc}' 
+      WHERE activated_at < '#{now_in_utc}' 
+      AND updated_at < activated_at"
+    )
+    puts "#{res.cmd_tuples()} #{tenant} call to actions updated in #{Time.now - start_time} seconds"
+  end
+
+  puts "All tenants call to actions updated"
+
+end
+
+main()
