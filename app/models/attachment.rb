44c0de7736f6f4958aee3fd15e5dda946af4ceb4
Filippo Ragazzo <filipporagazzo89@gmail.com>
2015-05-15 14:48:45 +0200

    [FD-1243] extra_field upload attachments styles set only for images

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -7,10 +7,18 @@ class Attachment < ActiveRecord::Base
   attr_accessor :destroy_data
 
   has_attached_file :data, 
-    :styles => { 
-      :carousel => "1024x320^", 
-      :medium => "524x393^", 
-      :thumb => "262x147^" 
+    styles: lambda { |image| 
+      if image.content_type =~ %r{^(image|(x-)?application)/(x-png|pjpeg|jpeg|jpg|png|gif)$}
+        {
+          :carousel => "1024x320^", 
+          :medium => "524x393^", 
+          :thumb => "262x147^"
+        }
+      elsif image.content_type =~ %r{^(image|(x-)?application)/(pdf)$}
+        { }
+      else
+        { }
+      end 
     }, 
     :convert_options => { 
       :carousel => " -crop '1024x320+0+40'", 
