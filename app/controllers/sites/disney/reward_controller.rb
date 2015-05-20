bad2dd4095b5027ef753c97378394f03dd840a1b
Filippo Ragazzo <filipporagazzo89@gmail.com>
2015-05-20 18:36:37 +0200

    [FD-1188] just added some documentation

--- app/controllers/sites/disney/reward_controller.rb
+++ app/controllers/sites/disney/reward_controller.rb
@@ -1,5 +1,7 @@
 class Sites::Disney::RewardController < RewardController
 
+  # RewardController get_property_for_reward_catalogue override method in order to build reward catalogue page 
+  # reward lists for all Disney properties
   def get_property_for_reward_catalogue
     nil
   end
