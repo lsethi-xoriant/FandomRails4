bd9afce71974f1a78838325ed1a700155af90de6
Alessandro Tolomio <atolomio@shado.tv>
2015-05-27 17:40:13 +0200

    [FD-1258] Added flexslider gem

--- lib/flexslider_workaround.rb
+++ lib/flexslider_workaround.rb
@@ -4,7 +4,7 @@ module Flexslider
   module Rails
     class Engine < ::Rails::Engine
       initializer :append_dependent_assets_path, :group => :all do |app|
-        app.config.assets.paths += %w( fonts )
+        app.config.assets.paths += %w( )
 
         app.config.assets.precompile += %w( flexslider.css )
         app.config.assets.precompile += %w( jquery.flexslider.js )
