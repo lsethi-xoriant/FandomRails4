8e4d61fa52ae43b6deb8d69b4ff84211f3b348da
Diego Dainese <ddainese@shado.tv>
2015-06-03 15:14:57 +0200

    [FD-1259] fixed ckeditor for rails 4

--- config/application.rb
+++ config/application.rb
@@ -67,6 +67,7 @@ module Fandom
     config.assets.initialize_on_precompile = false
 
     config.assets.precompile << /\.(?:svg|eot|woff|ttf|png|jpg)\z/
+    config.assets.precompile += %w( ckeditor/* )
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
