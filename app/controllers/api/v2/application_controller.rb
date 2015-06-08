05830038560cebafe3643d9b21bc20e9a3db4693
Matteo Pozzana <mpozzana@shado.tv>
2015-06-08 09:44:40 +0200

    [FIA-12] Added load more function for stream home

--- app/controllers/api/v2/application_controller.rb
+++ app/controllers/api/v2/application_controller.rb
@@ -23,8 +23,30 @@
         'call_to_action_highlight_list' => ctas_highlighted,
         'call_to_action_info_list_version' => get_max_updated_at_from_cta_info_list(calltoaction_info_list),
         'call_to_action_info_list_has_more' => has_more,
+        'menu_items' => get_menu_items(),
         # TODO: content section need to have their timestamp
         'content_sections' => []
+      }
+      
+      respond_with result.to_json
+    end
+    
+    def load_more_ctas_in_stream
+      cta_chunk_size = $site.init_ctas
+      cta_chunk_size = 10
+      
+      tag = get_property()
+      if tag
+        tag_name = tag.name
+      end
+
+      params = request.params
+      params["page_elements"] = ["like", "comment", "share"]
+      calltoaction_info_list, has_more = get_ctas_for_stream(tag_name, params, cta_chunk_size)
+      
+      result = {
+        'call_to_action_info_list' => calltoaction_info_list,
+        'call_to_action_info_list_has_more' => has_more
       }
       
       respond_with result.to_json
