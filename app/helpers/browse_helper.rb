774587ebdd13e76fb2eeca353b50d2b6f7ae2cff
Alessandro Tolomio <atolomio@shado.tv>
2015-06-16 15:26:44 +0200

    [FD-1300] Adjusted autocomplete search, removed ugc

--- app/helpers/browse_helper.rb
+++ app/helpers/browse_helper.rb
@@ -245,12 +245,12 @@ module BrowseHelper
     optional_category_tag_ids_condition = category_tag_ids.any? ? "AND tag_id in (#{category_tag_ids.join(",")})" : ""
     if tags.empty?
       tags = Tag.where("title ILIKE ? AND id IN (?) ","%#{term}%", category_tag_ids).limit(DEFAULT_BROWSE_ELEMENT_CAROUSEL)
-      ctas = CallToAction.active.where("title ILIKE ? AND user_id is null","%#{term}%").limit(DEFAULT_BROWSE_ELEMENT_CAROUSEL)
+      ctas = get_ctas(nil).where("call_to_actions.title ILIKE ? AND call_to_actions.user_id is null", "%#{term}%").limit(DEFAULT_BROWSE_ELEMENT_CAROUSEL)
     else
       tag_set = tags.map { |tag| "(select tag_id from tags_tags where other_tag_id = #{tag.id} #{optional_category_tag_ids_condition})" }.join(' INTERSECT ')
       cta_set = tags.map { |tag| "(select call_to_action_id from call_to_action_tags where tag_id = #{tag.id})" }.join(' INTERSECT ')
       tags = Tag.where("title ILIKE ? AND id IN (#{tag_set})","%#{term}%").limit(DEFAULT_BROWSE_ELEMENT_CAROUSEL)
-      ctas = CallToAction.active.where("title ILIKE ? AND id in (#{cta_set}) AND user_id is null","%#{term}%").limit(DEFAULT_BROWSE_ELEMENT_CAROUSEL)
+      ctas = get_ctas(nil).where("call_to_actions.title ILIKE ? AND call_to_actions.id in (#{cta_set}) AND call_to_actions.user_id is null", "%#{term}%").limit(DEFAULT_BROWSE_ELEMENT_CAROUSEL)
     end
     merge_contents_for_autocomplete(ctas, tags)
   end
