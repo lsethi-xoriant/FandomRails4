e7a2451c47d496c688f9fd3cce075fb5811cd512
Diego Dainese <ddainese@shado.tv>
2015-06-24 10:39:24 +0200

    [FIA-11] implemented api for facebook login

--- app/controllers/api/v2/user_controller.rb
+++ app/controllers/api/v2/user_controller.rb
@@ -24,4 +24,29 @@
       end
     end
 
+    def facebook_provider_name
+      "facebook_#{$site.id}"
+    end
+
+    def facebook_sign_in
+      token = params[:token]
+      token = "CAAJn9fHaIYgBALw5lX74nCPhyWLaCAEYeZBXZB6MujbA6weQT95lLAl1krZBZCrNqfqHNIlAvZCiqL12PMRA3QDDi4aszfopWWKZAegsedY6NJMdF8ABLIN2oYA3FY5Bq8hGucKC7nJk2hWnbs1x6XA6PixZC3rG90JJm5P0vwjpZAbnGxuLuG35M1gtzuFCJodu4dP4Ggox3JxA2k4fgOoQ"
+      koala = Koala::Facebook::API.new(token)
+      facebook_user = koala.get_object('me')
+
+      # TODO: handle authentications table
+      user = User.find_by_email(facebook_user["email"])
+      if user
+        respond_with_json user
+      else
+        password = Devise.friendly_token.first(8)
+        user = User.create(email: facebook_user["email"], first_name: facebook_user["first_name"], last_name: facebook_user["last_name"], password: password, password_confirmation: password)
+        if user.errors.blank?
+          respond_with_json user
+        else
+          respond_with_errors user.errors
+        end
+      end
+    end
+
 end
