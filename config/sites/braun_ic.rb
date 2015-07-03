6c7ea986adcc88323a2e1dc6e023c1137412661f
Alessandro Tolomio <atolomio@shado.tv>
2015-07-03 16:29:57 +0200

    [FD-1339] Adjusted assets precompile path

--- config/sites/braun_ic.rb
+++ config/sites/braun_ic.rb
@@ -4,6 +4,7 @@ module Fandom
       id: 'braun_ic',
       title: 'BraunIc',
       domains: ['braun-ic.fandom.localdomain', 'braun-ic.stage.fandomlab.com', 'braun-ic.dev.fandomlab.com', 'braun-ic.live.fandomlab.com'],
+      assets_precompile: ['braun_ic_application.css', 'braun_ic_application.js'],
       free_provider_share: true,
       interactions_for_anonymous: ["quiz", "share"],
       init_ctas: 3,
