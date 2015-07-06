2fb0f92fd0423b431a4d9356cfb50d168e898eea
Alessandro Tolomio <atolomio@shado.tv>
2015-07-06 11:27:15 +0200

    [FD-1359] Removed limit in fandom for anon interactions

--- config/sites/fandom.rb
+++ config/sites/fandom.rb
@@ -6,7 +6,7 @@ module Fandom
       domains: ['localhost', 'fandom.shado.tv', 'fandom.localdomain', 'stage.fandomlab.com', 'dev.fandomlab.com', 'live.fandomlab.com', 'demo.fandomlab.com', 'demo2.fandomlab.com'],
       twitter_integration: true,
       interactions_for_anonymous: ["quiz", "check", "play", "share", "vote", "like", 'pin'],
-      interactions_for_anonymous_limit: 5,
+      #interactions_for_anonymous_limit: 5,
       free_provider_share: true,
       default_property: "all",
       allowed_context_roots: ["all", "sport", "series", "talent", "music", "branded"],
