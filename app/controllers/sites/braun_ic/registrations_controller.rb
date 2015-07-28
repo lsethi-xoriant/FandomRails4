8f4d70650da239b5ff66890f08910bbf7530c7ec
Filippo Ragazzo <filipporagazzo89@gmail.com>
2015-07-28 12:41:23 +0200

    [FD-1413] braun registration mail

--- app/controllers/sites/braun_ic/registrations_controller.rb
+++ app/controllers/sites/braun_ic/registrations_controller.rb
@@ -6,4 +6,9 @@ class Sites::BraunIc::RegistrationsController < RegistrationsController
   def after_sign_up_path_for(resource)
     "/"
   end
+
+  def set_account_up
+    create_user_interaction_for_registration()
+    SystemMailer.welcome_mail_braun(current_user).deliver
+  end
 end
