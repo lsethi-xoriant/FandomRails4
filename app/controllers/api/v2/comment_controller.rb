35242d22e38943950417a09e54975791053efea7
Matteo Pozzana <mpozzana@shado.tv>
2015-07-29 10:11:49 +0200

    [FIA-22] CQ js & modified api for comment

--- app/controllers/api/v2/comment_controller.rb
+++ app/controllers/api/v2/comment_controller.rb
@@ -23,21 +23,38 @@
     
     user = current_or_anonymous_user
     
-    user_comment = UserCommentInteraction.create(user_id: user.id, approved: approved, text: user_text, comment_id: comment_resource.id, aux: aux)
-    response[:comment] = build_comment_for_comment_info(user_comment, true)
-    if approved && user_comment.errors.blank?
-      adjust_counter!(interaction, 1)
-      user_interaction, outcome = create_or_update_interaction(user, interaction, nil, nil)
+    if !anonymous_user?(user)
+      user_comment = UserCommentInteraction.create(user_id: user.id, approved: approved, text: user_text, comment_id: comment_resource.id, aux: aux)
+      response[:comment] = build_comment_for_comment_info(user_comment, true)
+      if approved && user_comment.errors.blank?
+        adjust_counter!(interaction, 1)
+        user_interaction, outcome = create_or_update_interaction(user, interaction, nil, nil)
+      end
+    else
+      captcha_enabled = get_deploy_setting("captcha", true)
+      response[:captcha] = captcha_enabled
+      response[:captcha_evaluate] = !captcha_enabled || params[:captcha_code] == Digest::MD5.hexdigest(params[:captcha_text] || "")
+      if response[:captcha_evaluate]
+        user_comment = UserCommentInteraction.create(user_id: current_or_anonymous_user.id, approved: approved, text: user_text, comment_id: comment_resource.id, aux: aux)
+        response[:comment] = build_comment_for_comment_info(user_comment, true)
+        if approved && user_comment.errors.blank?
+          user_interaction, outcome = create_or_update_interaction(user_comment.user, interaction, nil, nil)
+        end
+      end
+      response[:captcha] = generate_captcha_response
     end
 
     if user_comment && user_comment.errors.any?
       response[:errors] = user_comment.errors.full_messages.join(", ")
     end
 
-    respond_to do |format|
-      format.json { render :json => response.to_json }
-    end
+    respond_with response.to_json
 
   end    
+
+  def generate_captcha
+    response = generate_captcha_response
+    respond_with response.to_json
+  end
 
 end
