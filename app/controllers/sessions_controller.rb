638453427ff4e46ecdda1f55389fe7a1f5701656
Filippo Ragazzo <filipporagazzo89@gmail.com>
2015-08-06 12:07:32 +0200

    [CQ] removed "debugger" statement

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -78,7 +78,6 @@ class SessionsController < Devise::SessionsController
 
   # Authenticates and log in the user from the an oauth service
   def create_from_oauth
-    debugger
     if stored_anonymous_user?
       user = update_from_omniauth(env["omniauth.auth"], params[:provider])
     else
