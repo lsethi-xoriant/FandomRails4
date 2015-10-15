35bac6b3bfd71e1dd120c7bef702ba91ae5bd356
Matteo Pozzana <mpozzana@shado.tv>
2015-10-15 10:35:32 +0200

    [FIA-105] Bug fixing last badge obtained.
    Changed cache for reward image status. Removed cache short from
    get_reward_image_for_status, added expire cache in assign reward
    (CacheExpireHelper) and added cache_forever in
    get_user_rewards_from_cache

--- app/helpers/cache_expire_helper.rb
+++ app/helpers/cache_expire_helper.rb
@@ -2,6 +2,7 @@ module CacheExpireHelper
   def assign_reward_expires(user_id, reward_name)
     expire_cache_key(get_cta_to_reward_statuses_by_user_cache_key(user_id))
     expire_cache_key(get_status_rewar_image_key(reward_name, user_id))
+    expire_cache_key(get_user_rewards_cache_key(user_id))
   end
   
   def buy_reward_catalogue_expires(currency_name, user_id)
