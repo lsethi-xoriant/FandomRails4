9d55142cffa19a60b8097e866d20a39e05768483
Diego Dainese <ddainese@shado.tv>
2015-10-16 15:43:02 +0200

    [FD-513] added 2 domains for intesa

--- config/sites/intesa_expo.rb
+++ config/sites/intesa_expo.rb
@@ -2,7 +2,7 @@ module Fandom
   class Application < Rails::Application
     register_fandom_site(
       id: 'intesa_expo', 
-      domains: ['intesa-expo.fandom.localdomain', 'intesa-expo.live.fandomlab.com', 'intesa-expo.dev.fandomlab.com', 'intesa-expo.stage.fandomlab.com', 'expo2015.intesasanpaolo.com', 'expo.intesasanpaolo.com', 'imprese.unmondopossibile.com', 'www.expo2015.intesasanpaolo.com', 'www.expo.intesasanpaolo.com', 'www.imprese.unmondopossibile.com'],
+      domains: ['intesa-expo.fandom.localdomain', 'intesa-expo.live.fandomlab.com', 'intesa-expo.dev.fandomlab.com', 'intesa-expo.stage.fandomlab.com', 'expo2015.intesasanpaolo.com', 'expo.intesasanpaolo.com', 'imprese.unmondopossibile.com', 'www.expo2015.intesasanpaolo.com', 'www.expo.intesasanpaolo.com', 'www.imprese.unmondopossibile.com', 'www.italiadalvivo.com', 'italiadalvivo.com'],
       assets_precompile: ['intesa_expo_application.css', 'intesa_expo_application.js', 'intesa_expo_ie9.css'],
       periodicity_kinds: [PERIOD_KIND_DAILY],
       interactions_for_anonymous: ["share", "play", "download"],
