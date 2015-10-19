45d09a0922603eeb964c415e8449e9ef6c832d84
Filippo Ragazzo <filipporagazzo89@gmail.com>
2015-10-19 10:44:03 +0200

    [FD-1542] CQ

--- bin/instantwin_log_analyzer.rb
+++ bin/instantwin_log_analyzer.rb
@@ -115,7 +115,7 @@ def main
       AND tenant = '#{tenant}'"
     ).first["max"]
 
-    puts "#{Time.now} - max events timestamp retrieved (#{max_events_timestamp})"
+    puts "#{Time.now} - Max events timestamp retrieved (#{max_events_timestamp} UTC)"
 
     credits_assigned = exec_query(events_conn, tenant, events_is_tenant_specific, false, 
       "SELECT user_id, COUNT(*) FROM events WHERE 
