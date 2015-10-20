88b081dd7eb4ccf9e9b9996d5790f41eeea0f295
Filippo Ragazzo <filipporagazzo89@gmail.com>
2015-10-20 17:00:00 +0200

    [FD-1307] test active call to action visualization

--- test/unit/call_to_action_controller_test.rb
+++ test/unit/call_to_action_controller_test.rb
@@ -11,6 +11,13 @@ class CallToActionControllerTest < ActionController::TestCase
     @interaction = load_seed("call_to_action_controller")
   end
 
+  test "200 code for active call to action" do
+
+    visit(build_url_for_capybara("/call_to_action/312")) # Qual è la canzone più romantica della discografia dei Coldplay?
+    assert first("div[class='container main-container']"), "Call to action visualization problems"
+
+  end
+
   test "add comment for current user" do
 
     assert current_user, "Didn't find current_user"
