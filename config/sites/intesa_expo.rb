d9a14f0eff5f18851009b916c64b15e0e9b3ef2f
Alessandro Tolomio <atolomio@shado.tv>
2015-10-22 10:12:59 +0200

    [FD-1598] Added in italiadalvivo link interaction

--- config/sites/intesa_expo.rb
+++ config/sites/intesa_expo.rb
@@ -5,7 +5,7 @@ module Fandom
       domains: ['intesa-expo.fandom.localdomain', 'intesa-expo.live.fandomlab.com', 'intesa-expo.dev.fandomlab.com', 'intesa-expo.stage.fandomlab.com', 'expo2015.intesasanpaolo.com', 'expo.intesasanpaolo.com', 'imprese.unmondopossibile.com', 'www.expo2015.intesasanpaolo.com', 'www.expo.intesasanpaolo.com', 'www.imprese.unmondopossibile.com', 'www.italiadalvivo.com', 'italiadalvivo.com'],
       assets_precompile: ['intesa_expo_application.css', 'intesa_expo_application.js', 'intesa_expo_ie9.css'],
       periodicity_kinds: [PERIOD_KIND_DAILY],
-      interactions_for_anonymous: ["share", "play", "download"],
+      interactions_for_anonymous: ["share", "play", "download", "link"],
       allowed_context_roots: ["it", "en", "imprese", "inaugurazione"], # it and and are expo properties
       default_property: "it",
       free_provider_share: true,
