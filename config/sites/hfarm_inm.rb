95f45e689f9634814e10eee38bd2c9566310d71a
Diego Dainese <ddainese@shado.tv>
2015-10-23 16:46:31 +0200

    [FD-1611] new domain for ilnostromomento

--- config/sites/hfarm_inm.rb
+++ config/sites/hfarm_inm.rb
@@ -2,7 +2,7 @@ module Fandom
   class Application < Rails::Application
     register_fandom_site(
       id: 'hfarm_inm', 
-      domains: ['hfarm-inm.fandom.localdomain', 'hfarm-inm.live.fandomlab.com', 'hfarm-inm.dev.fandomlab.com', 'hfarm-inm.stage.fandomlab.com'],
+      domains: ['hfarm-inm.fandom.localdomain', 'hfarm-inm.live.fandomlab.com', 'hfarm-inm.dev.fandomlab.com', 'hfarm-inm.stage.fandomlab.com', 'ilnostromomento.com', 'www.ilnostromomento.com'],
       assets_precompile: ['hfarm_inm_application.css', 'hfarm_inm_application.js'],
       periodicity_kinds: [PERIOD_KIND_DAILY],
       free_provider_share: true,
